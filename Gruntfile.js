83f6630bebfa96fab6d399edd8aa0d1d725d1e56
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-09-09 14:59:56 -0700

    Initial structureFeatures: adding dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/*global module:false, require:false*/
+module.exports = function(grunt) {
+
+    "use strict";
+
+    var path = require('path'),
+        lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+        folderMount = function folderMount(connect, point) {
+            return connect.static(path.resolve(point));
+        };
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        watch: {
+            options: {
+                // Start a live reload server on the default port: 35729
+                livereload: true
+            },
+            spec: {
+                files: ['app/**/*']
+            }
+        },
+
+        connect: {
+            livereload : {
+                options : {
+                    port       : 9001,
+                    hostname: 'localhost',
+                    base       : './app/',
+                    middleware : function (connect, options) {
+                        return [lrSnippet, folderMount(connect, options.base)]
+                    }
+                }
+            }
+        },
+
+        open : {
+            reload : {
+                path : 'http://localhost:9001/'
+            }
+        },
+
+        copy: {
+            build: {
+                files: [
+                    {expand: true, cwd: 'app/', src: ['**'], dest: 'build'}
+                ]
+            }
+        },
+
+        build_gh_pages: {
+            ghPages: {
+                options: {
+                    build_branch: "gh-pages",
+                    dist: "build"
+                }
+            }
+        }
+
+    });
+
+    // To start editing your slideshow using livereload, run "grunt server"
+    grunt.registerTask("server", "Build and watch task", ["connect", "open", "watch"]);
+    grunt.registerTask("deploy", "Deploy to gh-pages", ["copy", "build_gh_pages"]);
+};
---
+++ Wiki/index.md
@@ -0,0 +1,23 @@
+# Grasshopper Admin
+
+## Dev Install
+
+* [Bower](http://bower.io/)
+
+## Versioning
+
+* [Semantic Versionining](http://semver.org/)
+    * x.y.z
+        * x backward incompatible changes
+        * y new features
+        * z bug fixes
+    * x.y.z-abc
+        * this comes before x.y.z
+
+## NPM
+
+* [package.json](https://npmjs.org/doc/json.html)
+
+## Notes:
+
+* Create grunt init template for SOLID projects (e.g. https://github.com/pajtai/grunt-init-reveal-jade)
