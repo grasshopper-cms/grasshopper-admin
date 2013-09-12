8ed58e4e8f228a3e7de3b212132c59d5cf11393f
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-09-11 17:36:50 -0700

    Working on Backbone Rendering.

--- app/views/base/baseView.js
+++ app/views/base/baseView.js
@@ -1,8 +1,12 @@
 define(['backbone'], function (Backbone) {
 
     var BaseView = Backbone.View.extend({
-
+        // renderToStage : renderToStage
     });
+
+    // function renderToStage() {
+    //     $('#stage').html(_.template(this.template, data));
+    // };
 
     return BaseView;
 
--- app/views/login/loginView.js
+++ app/views/login/loginView.js
@@ -1,9 +1,20 @@
-define(['baseView', 'rivetView'], function (BaseView, rivetView) {
+define(['baseView', 'rivetView', 'text!views/login/loginView.html']
+    , function (BaseView, rivetView, templateHtml) {
 
     var LoginView = BaseView.extend({
         model : new Backbone.Model({name: 'Joe'}),
+        template : _.template(templateHtml),
         rivetView : rivetView({rivetScope: '#login', rivetPrefix: 'login'})
     });
+
+    // console.log(typeof LoginView);
+    // console.log(LoginView.hasOwnProperty('model'));
+    // console.log(Object.keys(LoginView));
+    // LoginView.renderToStage();
+    // var duder = function() {
+    //     $('#stage').html(LoginView.template);
+    // }();
+
 
     return LoginView;
 });
