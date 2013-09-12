2e9ebe224ba3fc83a651bcf7ec6a674e7ab59b16
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-09-12 11:18:04 -0700

    #19380 - BugFixes: Initialize Method for BaseView to handle template data.

--- app/views/base/baseView.js
+++ app/views/base/baseView.js
@@ -2,7 +2,13 @@ define(['backbone'], function (Backbone) {
 
     var BaseView = Backbone.View.extend({
         render : function () {
-            this.$el.html(this.template(this.templateData))
+            this.$el.html(this.template(this.dataToJSON()))
+        },
+        dataToJSON : function() {
+            return this.model ? this.model.toJSON() : {};
+        },
+        initialize : function() {
+            this.template = _.template(this.templateHtml);
         }
     });
 
