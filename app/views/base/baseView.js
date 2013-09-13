a222cbcd543431c602db7c1c527724ff469b9f45
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-09-12 19:50:59 -0700

    #19373 New Features: adding life cycle method calls and todos

--- app/views/base/baseView.js
+++ app/views/base/baseView.js
@@ -1,38 +1,61 @@
 define(['backbone', 'underscore', 'channels'], function (Backbone, _, channels) {
 
     var BaseView = Backbone.View.extend({
-        options: {
-            name:"BaseView"
+        options : {
+            name : "BaseView"
         },
-        start : function() {
-            var $deferred = new $.Deferred();
-            $.when(function() {
-                // TODO: Refactor this.
-                var event = this.options.name + ":beforeRender",
-                    promise;
-                this.channels.views.trigger(event);
-                if (this.beforeRender) {
-                    promise = this.beforeRender();
-                }
-                return promise;
-                }.bind(this)())
-                .done(function() {
-                    var event = this.options.name + ":render";
-                    this.channels.views.trigger(event)
-                    this.render();
-                }.bind(this));
-            return $deferred.promise();
-        },
-        render : function() {
-            this.$el.html(this.template(this.dataToJSON()));
-        },
-        dataToJSON : function() {
-            return this.model ? this.model.toJSON() : {};
-        },
-        initialize : function() {
-            this.template = _.template(this.templateHtml);
-        }
+        initialize : initialize,
+        start : start,
+        render : render,
+        dataToJSON : dataToJSON
     });
+
+    function start () {
+        var $deferred = new $.Deferred(),
+            self = this;
+        $
+            .when(_beforeRender.call(this))
+            .done(_render.call(this))
+            //TODO: implement _afterRender
+            .done(_resolveStart.call(this, $deferred));
+        //TODO: implement fail method that is called if before or after render is rejected
+        return $deferred.promise();
+    }
+
+    function render () {
+        this.$el.html(this.template(this.dataToJSON()));
+    }
+
+    function dataToJSON () {
+        return this.model ? this.model.toJSON() : {};
+    }
+
+    function initialize () {
+        this.template = _.template(this.templateHtml);
+    }
+
+    // Share channels among all Views
     BaseView.prototype.channels = channels;
+
+    // --------------------------
+    // Private Methods
+
+    function _beforeRender () {
+        // TODO: create a method to generate the event name from the views name + the event name
+        var event = this.options.name + ":onBeforeRender";
+        this.channels.views.trigger(event);
+        return this.beforeRender ? this.beforeRender() : undefined;
+    }
+
+    function _render () {
+        var event = this.options.name + ":render";
+        this.channels.views.trigger(event)
+        this.render();
+    }
+
+    function _resolveStart ($deferred) {
+        $deferred.resolve();
+    }
+
     return BaseView;
 });
--- tests/views/baseView.js
+++ tests/views/baseView.js
@@ -27,74 +27,99 @@ define(['chai', 'squire', 'mocha'], function (chai, Squire, mocha) {
             should.exist(BaseView);
         });
 
-        describe("start method", function() {
-            it("should exist", function() {
+        describe("start method", function () {
+            it("should exist", function () {
                 should.exist(viewInstance.start);
             });
-            it('should be a function', function() {
+            it('should be a function', function () {
                 viewInstance.start.should.be.a('function');
             });
-            it('should return a promise', function() {
+            it('should return a promise', function () {
                 var promise = viewInstance.start();
                 promise.should.have.property('done');
                 promise.should.not.have.property('resolve');
             });
-        });
-        describe("render method", function() {
-            it("should exist", function() {
-                should.exist(viewInstance.render);
+            it('the promise should be resolved after start runs', function () {
+                var listener = function () {
+
+                };
+                viewInstance.start().done(listener);
+                //TODO: use sinon to spy on listener and make sure it was called
             });
-            it('should be a function', function() {
-                viewInstance.render.should.be.a('function');
+            it('the promise should be rejected if beforeRender fails', function () {
+
             });
-        });
-        describe("beforeRender method", function() {
-            it("should trigger the beforeRender event on the view's channel", function(done) {
-                viewInstance.channels.views.on("BaseView:beforeRender", function() {
-                    true.should.be.true;
-                    done();
+            it('the promise should be rejected if afterRender fails', function () {
+
+            });
+            describe("beforeRender method", function () {
+                it("should trigger the onBeforeRender event on the view's channel", function (done) {
+                    viewInstance.channels.views.on("BaseView:onBeforeRender", function () {
+                        //TODO: use a sinon spy to check whether this anon function was called (use sinon-chai)
+                        true.should.be.true;
+                        done();
+                    });
+                    viewInstance.start();
                 });
-                viewInstance.start();
+            });
+            describe("render method", function () {
+                it("should exist", function () {
+                    should.exist(viewInstance.render);
+                });
+                it('should be a function', function () {
+                    viewInstance.render.should.be.a('function');
+                });
+                it("should trigger the onRender event on the view's channel", function () {
+
+                });
+                describe("- if the beforeRender method returns a promise -", function () {
+                    beforeEach(function () {
+                        //TODO:create a base view that return a promise for beforeRender
+                    });
+                    it("the render method will only fire after the promise is resolved", function () {
+
+                    });
+                });
+                describe("- if the beforeRender method doesn't return a promise-", function () {
+                    it("the render method should fire immediately", function () {
+
+                    });
+                });
+            });
+            describe("afterRender method", function () {
+                it("should trigger the onAfterRender event on the view's channel", function () {
+
+                });
+            });
+        });
+
+        describe("stop method", function () {
+            // TODO: implement before aftter etc methods in the same was as for render
+            // method should wrap View.remove
+        });
+
+        describe("channels", function () {
+            it("can be triggered by one view and heard by another", function () {
+                //TODO: create a second view instance
             });
         });
     });
 
 });
 
-// Each life cycle method
-//    calls some methods
-//    returns a promise that is resolved when the method completes
-//    triggers events on channels.views using its name as
-//      the namespace and the promise as an argument
-//
-//        channels.views.trigger("loginView:beforeRender", promise);
-// e.g.
-// .render() will call the methods this.beforeRender, run itself, this.afterRender
-// each lifecycle method will return a promise that is fulfilled after the method runs
-/*
-
-lifeCycleMethod: function() {
-    var $deferred = $.Deferred();
-    // This is basically a timeout, 0
-    _.defer(function() {
-        ...
-    });
-    return $deferred.promise();
-}
-
-$deferred.resolve()
-$deferred.reject()
-
-*/
 // Example Methods:
 
 // Setup Methods
-// AfterInitialize
-// BeforeRender
-// Render
-// RenderChildren
-// AfterRenderChildren
-// AfterRender
-// BindEvents
-// TearDown
-// Destroy
+// initialize (BB method)
+//  afterInitialize
+// start
+//  bindEvents
+//  beforeRender
+//  render
+//  beforeRenderChildren
+//  renderChildren
+//  afterRenderChildren
+//  afterRender
+// stop
+//  beforeRemove
+//  remove (BB method)
