88ce8c365103aeb30d0533d6f11389078afe0b3b
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-09-19 17:50:26 -0700

    GRAS-93 Establish View Lifecycle Features: Test file is now running without errors for LoginView.

--- tests/main.js
+++ tests/main.js
@@ -15,10 +15,12 @@ require.config({
         }
     },
     paths : {
+        // Libraries
         underscore : '../app/vendor/lodash/dist/lodash.underscore',
         jquery : '../app/vendor/jquery/jquery',
         backbone : '../app/vendor/backbone-amd/backbone',
         text : '../app/vendor/requirejs-text/text',
+        rivets : '../app/vendor/rivets/dist/rivets',
         mocha : '../app/vendor/mocha/mocha',
         chai : '../app/vendor/chai/chai',
         squire : '../app/vendor/squire/src/Squire',
@@ -28,13 +30,35 @@ require.config({
         sinonCall : '../app/vendor/sinon/lib/sinon/call',
 
         // Views
+        baseView : '../app/vendor/masseuse/app/baseView',
         loginView : '../app/views/login/loginView',
-
-        // Channels
-        channels : '../app/channels',
+        loginViewConfig : '../app/views/login/loginViewConfig',
+        navbarView : '../app/views/navbar/navbarView',
+        navbarViewConfig : '../app/views/navbar/navbarViewConfig',
+        emptyView : '../app/views/empty/emptyView',
+        emptyViewConfig : '../app/views/empty/emptyViewConfig',
 
         // Mixins
-        mixin : '../app/mixins/mixin'
+        mixin : '../app/vendor/masseuse/app/mixin',
+        rivetView : '../app/mixins/rivetView',
+        loginMixin : '../app/mixins/loginMixin',
+
+        // Channels
+        channels : '../app/vendor/masseuse/app/channels',
+
+        // Models
+        selfValidatingModel : '../app/models/selfValidatingModel',
+        userModel : '../app/models/userModel',
+
+        // View Models
+        loginViewModel : '../app/models/viewModels/loginViewModel',
+        navbarViewModel : '../app/models/viewModels/navbarViewModel',
+
+        // Workers
+        loginWorker : '../app/workers/loginWorker',
+
+        // Api proxy
+        api : '../app/api/api'
     }
 });
 
