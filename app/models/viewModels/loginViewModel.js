4ff7d783654fcf92fd7cb1d1e94f5595f54c1d24
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-09-21 03:49:53 -0700

    updating things so ComputedProperties is working

--- app/models/viewModels/loginViewModel.js
+++ app/models/viewModels/loginViewModel.js
@@ -1,15 +1,13 @@
-define(['selfValidatingModel', 'validation', 'ComputedProperty'], function (Model, validation, ComputedProperty) {
+define(['masseuseModel', 'validation'], function (Model, validation) {
     return Model.extend({
 
         validate: validate,
         setErrors : setErrors,
-        attributeValidate : attributeValidate,
-        hasError: ComputedProperty(["usernameError", "passwordError"], function (usernameError, passwordError) {
-            return !!(usernameError || passwordError);
-        })
+        attributeValidate : attributeValidate
     });
 
     function validate(attributes, options) {
+        // We can't just check 'hasError', since it is not set initially, so we have to look through the model
         var valid = this.attributeValidate(attributes.username, 'usernameError');
         valid = this.attributeValidate(attributes.password, 'passwordError') && valid;
         return !valid;
--- app/views/login/loginViewConfig.js
+++ app/views/login/loginViewConfig.js
@@ -1,11 +1,15 @@
 /*global define:false*/
-define(['text!views/login/loginView.html', 'loginViewModel'], function (templateHtml, loginViewModel) {
+define(['text!views/login/loginView.html', 'loginViewModel', 'ComputedProperty'], function (templateHtml, loginViewModel, ComputedProperty) {
     'use strict';
 
     return {
         name : 'loginView',
         // for model attribute errors on self validating models, follow the convention of attribute+"Error". See the rivetView "publish" method
-        modelData : {username : '', password : '', usernameError : '', passwordError : '', hasError : ''},
+        modelData : {username : '', password : '', usernameError : '', passwordError : '',
+            // hasError is used to great out the submit box
+            hasError : new ComputedProperty(['usernameError', 'passwordError'], function(usernameError, passwordError) {
+                return !!(usernameError || passwordError);
+            })},
         ModelType : loginViewModel,
         el : '#stage',
         templateHtml : templateHtml,
