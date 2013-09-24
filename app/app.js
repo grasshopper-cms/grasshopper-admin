41f0d4ee010735f9263e610c8e1da6c1ccb8f956
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2013-09-24 14:26:05 -0700

    Corrections for header view

--- app/app.js
+++ app/app.js
@@ -1,16 +1,15 @@
 define(['UserModel', 'underscore', 'backbone'],
     function(Usermodel, _, Backbone) {
-        // TODO: app is not a global, it will return a new Usermodel with each dependency inclusion
-        // For things that are shared among just a few views, inject the dependency on initialize
-        // For things that are shared among all view, add the dependency to the prototype during an initial
-        // customization method (see CustomizeBackbone in Focus@Will for examples)
+
+        var app = {};
 
 
-        var app = {
-            user: new Usermodel()
+
+        return function(){
+            if (! app) {
+                app.user = new UserModel();
+                _.extend(app, Backbone.Events);
+            }
+            return app;
         };
-
-        _.extend(app, Backbone.Events);
-
-        return app;
 });
