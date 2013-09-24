9680942753c5c50793604d4aa14f4666c4cbfd74
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2013-09-24 14:26:48 -0700

    Revert "Corrections for header view"
    This reverts commit 41f0d4ee010735f9263e610c8e1da6c1ccb8f956.

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
 define(['UserModel', 'underscore', 'backbone'],
     function(Usermodel, _, Backbone) {
+        // TODO: app is not a global, it will return a new Usermodel with each dependency inclusion
+        // For things that are shared among just a few views, inject the dependency on initialize
+        // For things that are shared among all view, add the dependency to the prototype during an initial
+        // customization method (see CustomizeBackbone in Focus@Will for examples)
 
-        var app = {};
 
-
-
-        return function(){
-            if (! app) {
-                app.user = new UserModel();
-                _.extend(app, Backbone.Events);
-            }
-            return app;
+        var app = {
+            user: new Usermodel()
         };
+
+        _.extend(app, Backbone.Events);
+
+        return app;
 });
