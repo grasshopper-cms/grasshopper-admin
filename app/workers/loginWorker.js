1aee4f2eba3098be674413f15c396813f77d606d
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2013-10-01 15:40:05 -0700

    minor fix to router

--- app/workers/loginWorker.js
+++ app/workers/loginWorker.js
@@ -14,9 +14,24 @@ define(['api', 'jquery', 'emptyView', 'emptyViewConfig', 'resources', 'UserModel
                 .done(function (data) {
                     if ('Token' === data.token_type) {
                         localStorage.authToken = data.access_token;
-                        loginView.model.clear();
-                        loginView.app.router.navigate('home', {trigger: true});
                     }
+                    api.authenticateToken(localStorage.authToken)
+                        .done(function (data) {
+                            loginView.model.clear();
+                            loginView.app.user.set({
+                                _id : data._id,
+                                email : data.email,
+                                enabled : data.enabled,
+                                login : data.login,
+                                name : data.name,
+                                password : data.password,
+                                role : data.role
+                            });
+                            loginView.app.router.navigateTrigger('home');
+                        })
+                        .fail(function () {
+                            self.navigateTrigger('login');
+                        });
                 })
                 .fail(function (xhr) {
                     loginView.throwLoginError(resources.api.login.errors[xhr.status]);
