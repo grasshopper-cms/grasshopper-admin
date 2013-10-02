641d1080aaf9df08dbeb6877681d8eec67048722
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2013-10-02 11:52:19 -0700

    GRAS-197 clicking enter now submits the form.

--- app/models/viewModels/usersIndexViewModel.js
+++ app/models/viewModels/usersIndexViewModel.js
@@ -15,46 +15,36 @@ define(['masseuseModel', 'computedProperty'], function (Model, ComputedProperty)
                 return attribute.grab('pages');
             }, true),
             nextPage : new ComputedProperty(['users'], function (attribute) {
-                return calculateNextButton(attribute);
+                return calculatePrevOrNextButton(attribute, 'next');
             }, true),
             prevPage : new ComputedProperty(['users'], function (attribute) {
-                return calculatePrevButton(attribute);
+                return calculatePrevOrNextButton(attribute, 'prev');
             }, true)
-
         }
     });
 
-    function calculateNextButton (collection) {
+    function calculatePrevOrNextButton (collection, button) {
         var returnObj = {
-                disabled : true,
-                link : '#users',
-                onClick : 'return false;'
-            };
-
-        if (collection.grab('currentPage') < collection.grab('totalPages')) {
-            returnObj = {
-                disabled : false,
-                link : '#users/page/' + (collection.grab('currentPage') + 1),
-                onClick : ''
-            };
-        }
-
-        return returnObj;
-    }
-
-    function calculatePrevButton (collection) {
-        var returnObj = {
-                disabled : true,
-                link : '#users',
-                onClick : 'return false;'
-            };
-
-        if (collection.grab('currentPage') > 1) {
-            returnObj = {
-                disabled : false,
-                link : '#users/page/' + (collection.grab('currentPage') - 1),
-                onClick : ''
-            };
+            disabled : true,
+            link : '#users',
+            onClick : 'return false;'
+        };
+        if ('prev' == button) {
+            if (collection.grab('currentPage') > 1) {
+                returnObj = {
+                    disabled : false,
+                    link : '#users/page/' + (collection.grab('currentPage') - 1),
+                    onClick : ''
+                };
+            }
+        } else {
+            if (collection.grab('currentPage') < collection.grab('totalPages')) {
+                returnObj = {
+                    disabled : false,
+                    link : '#users/page/' + (collection.grab('currentPage') + 1),
+                    onClick : ''
+                };
+            }
         }
 
         return returnObj;
