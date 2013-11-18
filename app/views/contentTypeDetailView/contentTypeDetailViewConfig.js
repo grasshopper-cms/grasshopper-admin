7b8bfd5bc584aa312afd697f1623607919cbc0bc
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-11-18 13:50:54 -0800

    Submodule Update.

---
+++ app/views/contentTypeDetailView/contentTypeDetailViewConfig.js
@@ -0,0 +1,25 @@
+/*global define:false*/
+define(['text!views/userDetail/userDetailView.html', 'text!views/userDetail/_userDetailRow.html', 'userDetailViewModel', 'resources', 'constants'], function (formTemplate, rowTemplate, userDetailViewModel, resources, constants) {
+    'use strict';
+
+    return {
+        name : 'userDetailView',
+        ModelType : userDetailViewModel,
+        modelData: {},
+        el : '#stage',
+        templateHtml : formTemplate,
+        events : {
+            'click #saveUser' : 'updateModel',
+            'click .toggleEnabled' : 'toggleEnabled'
+        },
+        appendView : true,
+        bindings : [],
+        rivetConfig : 'auto',
+        mastheadButtons : [
+                                {
+                                    text : resources.mastheadButtons.addNewUser,
+                                    href : '#' + constants.api.newUser.shortUrl
+                                }
+                          ]
+    };
+});
