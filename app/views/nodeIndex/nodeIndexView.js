51364c75a07757db648af81d6324a8fdaca5b4b2
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-11-21 15:43:47 -0800

    Node Detail rows are working. need to rivet them.

--- app/views/nodeIndex/nodeIndexView.js
+++ app/views/nodeIndex/nodeIndexView.js
@@ -1,24 +1,43 @@
 /*global define:false*/
-define(['baseView'], function (BaseView) {
+define(['baseView', 'nodeDetailView', 'nodeDetailViewConfig', 'underscore', 'text!views/nodeDetail/_nodeDetailRow.html'], function (BaseView, NodeDetailView, nodeDetailViewConfig, _, nodeDetailRowTemplate) {
     'use strict';
 
     var nodeIndexView = BaseView.extend({
-        beforeRender: beforeRender
+        beforeRender: beforeRender,
+        appendNodeDetailRow : appendNodeDetailRow
     });
 
     function beforeRender() {
         var self = this;
 
+        // determines if this is the root or not (if it is root then the nodeId will be null)
         if(this.options.nodeId) {
             this.model.url = this.model.url.replace(':id', this.options.nodeId);
+            this.options.root = false;
         } else {
             this.model.url = this.model.url.replace(':id', 0);
+            this.options.root = true;
         }
 
         this.model.fetch()
             .done(function() {
-                console.log(self.model);
+                _.each(self.model.attributes, function(node) {
+                    if(_.has(node, '_id')) {
+                        self.appendNodeDetailRow(node);
+                    }
+                });
             });
+    }
+
+    function appendNodeDetailRow(node) {
+        var nodeDetailView = new NodeDetailView(_.extend({}, nodeDetailViewConfig,
+            {
+                name: 'nodeDetailRow',
+                modelData: node,
+                el: '#nodeDetailRow',
+                templateHtml: nodeDetailRowTemplate
+            }));
+        nodeDetailView.start();
     }
 
     return nodeIndexView;
