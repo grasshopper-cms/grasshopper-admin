d817f267743ae6a89783328e5664ee6fddeb8f4b
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-11-21 12:11:41 -0800

    Adding urlRoot functionality to the contentIndexView.

--- app/models/viewModels/contentIndexViewModel.js
+++ app/models/viewModels/contentIndexViewModel.js
@@ -1,8 +1,8 @@
-define(['grasshopperModel', 'resources'], function (Model, resources) {
+define(['grasshopperModel', 'resources', 'constants'], function (Model, resources, constants) {
     return Model.extend({
         defaults: {
             resources:resources
-        }
+        },
+        urlRoot: constants.api.nodes.url
     });
-
 });
--- app/views/contentIndex/contentIndexView.js
+++ app/views/contentIndex/contentIndexView.js
@@ -3,13 +3,20 @@ define(['baseView', 'jquery'], function (BaseView, $) {
     'use strict';
 
     var ContentIndexView = BaseView.extend({
+        beforeRender: beforeRender,
         afterRender: afterRender
     });
 
+    function beforeRender() {
+        this.model.fetch()
+            .done(function() {
+                console.log(this.mode);
+            });
+    }
+
     function afterRender() {
-
+        //TODO: What is this and what is it doing? maybe it should be moved.
         $(document).foundation('section', 'reflow');
-
     }
 
 
