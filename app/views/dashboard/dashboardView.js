b3c1aadc973a43e748bfa3ecbd5d6fbf223e559b
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-12-18 11:45:14 -0800

    Removed Cirque Reference.

--- app/views/dashboard/dashboardView.js
+++ app/views/dashboard/dashboardView.js
@@ -1,19 +1,7 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'jquery'], function (GrasshopperBaseView, $) {
+define(['grasshopperBaseView'], function (GrasshopperBaseView) {
     'use strict';
 
     return GrasshopperBaseView.extend({
-        afterRender: afterRender
     });
-
-    function afterRender() {
-        // TODO: What is this and what is it doing?
-        $('.report-cirque').cirque ({
-            radius: 60,
-            total: 7630,
-            lineWidth: 10,
-            trackColor: '#CCCCCC'
-        });
-    }
-
 });
