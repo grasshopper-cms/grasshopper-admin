11068702a82772b53fad11a06e4753e029497d84
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2013-12-19 18:34:39 -0800

    Refactored the AddFolderView getFileName Method.

--- app/views/addFolder/addFolderView.js
+++ app/views/addFolder/addFolderView.js
@@ -19,66 +19,98 @@ define(['grasshopperBaseView', 'resources', 'contentTypeWorker', 'nodeWorker'],
                     type : 'input'
                 })
                 .done(function (modalData) {
-                    createFolder.call(self, modalData.data)
+                    _createFolder.call(self, modalData.data)
                         .done(function () {
-                            self.displayTemporaryAlertBox(
-                                {
-                                    msg : resources.node.successfullyCreated,
-                                    status : true
-                                }
-                            );
-                            self.channels.views.trigger('refreshContentBrowseView');
-                            contentTypeWorker.getAvailableContentTypes()
-                                .done(function (availableContentTypes) {
-                                    self.displayModal(
-                                        {
-                                            msg : resources.contentType.addContentTypes,
-                                            type : 'checkbox',
-                                            data : availableContentTypes
-                                        })
-                                        .done(function (modalData) {
-                                            contentTypeWorker
-                                                .addContentTypesToFolder(self.model.get('nodeId'), modalData.data)
-                                                .done(function () {
-                                                    self.displayTemporaryAlertBox(
-                                                        {
-                                                            msg : resources.contentType.contentTypeAdded,
-                                                            status : true
-                                                        }
-                                                    );
-                                                })
-                                                .fail(function (msg) {
-                                                    self.displayTemporaryAlertBox(
-                                                        {
-                                                            msg : msg
-                                                        }
-                                                    );
-                                                });
-                                        })
-                                        .always(function () {
-                                            navigateBack.call(self);
-                                        });
-                                });
+                            _handleSuccessfulFolderCreation.call(self);
+                            _prepareToAddContentTypesToFolder.call(self);
                         })
                         .fail(function () {
-                            self.displayTemporaryAlertBox(
-                                {
-                                    msg : resources.node.errorCreating
-                                }
-                            );
+                            _handleFailedFolderCreation.call(self);
                         });
                 })
                 .fail(function () {
-                    navigateBack.call(self);
+                    _navigateBack.call(self);
                 });
         }
 
+        function _handleSuccessfulFolderCreation() {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.node.successfullyCreated,
+                    status : true
+                }
+            );
+            this.channels.views.trigger('refreshContentBrowseView');
+        }
 
-        function createFolder (folderName) {
+        function _handleFailedFolderCreation() {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.node.errorCreating
+                }
+            );
+        }
+
+        function _prepareToAddContentTypesToFolder() {
+            var self = this;
+
+            _getAvailableContentTypes.call(this)
+                .done(function (availableContentTypes) {
+                    _askUserWhatContentTypesToAttach.call(self, availableContentTypes)
+                        .done(function (modalData) {
+                            _actuallyAddContentTypesToFolder.call(self, modalData.data)
+                                .done(function () {
+                                    _handleSuccessfulContentTypeAdd.call(self);
+                                })
+                                .fail(function (msg) {
+                                    _handleFailedContentTypeAdd.call(self, msg);
+                                });
+                        })
+                        .always(function () {
+                            _navigateBack.call(self);
+                        });
+                });
+        }
+
+        function _handleSuccessfulContentTypeAdd() {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.contentType.contentTypeAdded,
+                    status : true
+                }
+            );
+        }
+
+        function _handleFailedContentTypeAdd(msg) {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : msg
+                }
+            );
+        }
+
+        function _actuallyAddContentTypesToFolder(selectedContentTypes) {
+            return contentTypeWorker.addContentTypesToFolder(this.model.get('nodeId'), selectedContentTypes);
+        }
+
+        function _askUserWhatContentTypesToAttach(availableContentTypes) {
+            return this.displayModal(
+                {
+                    msg : resources.contentType.addContentTypes,
+                    type : 'checkbox',
+                    data : availableContentTypes
+                });
+        }
+
+        function _getAvailableContentTypes() {
+            return contentTypeWorker.getAvailableContentTypes();
+        }
+
+        function _createFolder (folderName) {
             return nodeWorker.createFolder(this.model.get('nodeId'), folderName);
         }
 
-        function navigateBack (trigger) {
+        function _navigateBack (trigger) {
             this.app.router.navigateBack(trigger);
             this.app.router.removeThisRouteFromBreadcrumb();
             this.remove();
