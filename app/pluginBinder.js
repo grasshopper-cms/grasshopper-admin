d0041a5437133e9a16c66c4a10527eb82a273005
Main Conf Room <poklk@dfsw;lkj;wflk/cp.com>
2014-01-13 12:19:22 -0800

    plugins using proxyProperty commit - test commit

--- app/pluginBinder.js
+++ app/pluginBinder.js
@@ -1,27 +1,31 @@
 /* jshint loopfunc:true */
-define(['jquery', 'underscore', 'plugins', 'require'], function ($, _, plugins, require) {
-    'use strict';
+define(['jquery', 'underscore', 'plugins', 'require', 'proxyProperty'],
+    function ($, _, plugins, require, ProxyProperty) {
+        'use strict';
 
 
-    return {
-        fieldtype : function(el, field) {
-            var plugin = _.find(plugins.fields, {type : field.type});
+        return {
+            fieldtype : function(el, field) {
+                var plugin = _.find(plugins.fields, {type : field.type}),
+                    rivets = this;
 
 //            console.log(el);
 //            console.log(field);
 //            console.log(this);
-            console.log(this.view.models.view.model.attributes);
+                console.log(this.view.models.view.model);
 
-            require([plugin.view, plugin.config], function(ViewModule, configModule) {
+                require([plugin.view, plugin.config], function(ViewModule, configModule) {
 
-                var viewInstance = new ViewModule(_.extend({}, configModule, {
-                    modelData : _.extend({}, field, {}),
-                    el : el
-                }));
+                    var viewInstance = new ViewModule(_.extend({}, configModule, {
+                        modelData : {
+                            value: ProxyProperty('fields.' + field._id, rivets.view.models.view.model)
+                        },
+                        el : el
+                    }));
 
-                viewInstance.start();
-            });
-        }
-    };
+                    viewInstance.start();
+                });
+            }
+        };
 
-});
+    });
