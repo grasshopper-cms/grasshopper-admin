0bd5c63ef0230a0d5b0661b657b4fab7d1ed8bd2
Main Conf Room <poklk@dfsw;lkj;wflk/cp.com>
2014-01-21 11:17:52 -0800

    Progress Commit. More support for Multi fields.

--- app/grasshopperBinders.js
+++ app/grasshopperBinders.js
@@ -12,10 +12,35 @@ define(['jquery', 'underscore', 'masseuse',
                     viewInstance;
 
                 viewInstance = new PluginWrapperView(_.extend({}, pluginWrapperViewConfig, {
-                    modelData : _.extend({}, field, {
-                        value: masseuse.ProxyProperty('fields.' + field._id, rivets.view.models.view.model)
-                    }),
-                    collection : new Backbone.Collection([], {}),
+                    modelData : _.extend({}, field),
+                    collection : new (Backbone.Collection.extend({
+                        initialize: function (models, options) {
+                            var inModels = models;
+
+                            // Ensure that we are dealing with an array
+                            if (!inModels instanceof Array) {
+                                inModels = [models];
+                            }
+
+                            Backbone.Collection.prototype.initialize.call(this, inModels, options);
+
+                            this.listenTo(rivets.view.models.view.model, 'change', function () {
+                                // Update the collection here with the new data from the server
+                                this.reset(rivets.view.models.view.model.get('fields.' + field._id), {silent: true});
+                            });
+
+                            this.on('add remove reset change', function () {
+                                // Update the parent model value
+                                rivets.view.models.view.model.set('fields.' + field._id, this.toJSON());
+                            });
+                        },
+                        toJSON: function () {
+                            // If this is a collection with only one model, return the object not wrapped in an array
+                            var json = Backbone.Collection.prototype.toJSON.apply(this);
+
+                            return _.pluck(json, 'value');
+                        }
+                    }))([], {}),
                     appendTo : el
                 }));
                 rivets.view.models.view.addChild(viewInstance);
