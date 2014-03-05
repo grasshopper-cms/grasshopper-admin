31ffc3fa13e7ba8540ae4b59c015695a444d2aba
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-05 14:41:37 -0800

    Content Type fields are now sortable.

--- app/views/contentTypeDetail/contentTypeDetailView.js
+++ app/views/contentTypeDetail/contentTypeDetailView.js
@@ -32,6 +32,8 @@ define(['grasshopperBaseView', 'contentTypeDetailViewConfig',
         this.$el.foundation();
         this.listenTo(this.collection, 'change:type', _warnUserBeforeChangingType);
 
+        _initializeSortableAccordions.call(this);
+
         _addClickListenersToAccordion.call(this);
     }
 
@@ -205,6 +207,24 @@ define(['grasshopperBaseView', 'contentTypeDetailViewConfig',
                 $currentTarget.addClass('activeHeader');
             }
         });
+    }
+
+    function _initializeSortableAccordions() {
+        var $sortable = this.$('.accordion');
+        $sortable.sortable({
+            stop : _applyCollectionSort.bind(this, $sortable)
+        });
+    }
+
+    function _applyCollectionSort($sortable) {
+        var fields = [],
+            self = this;
+
+        $sortable.find('.accordionHeader').each(function() {
+            fields.push(self.collection.get($(this).attr('modelid')));
+        });
+
+        this.collection.reset(fields, { silent : true });
     }
 
 });
