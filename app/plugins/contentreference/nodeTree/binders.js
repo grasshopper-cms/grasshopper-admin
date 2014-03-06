b8eb2ed937f5b2b4f0d62c96dc6ba7bb9acd3512
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-05 17:52:23 -0800

    Fixed bug in content reference binder where additional API calls were being made for no reason.

--- app/plugins/contentreference/nodeTree/binders.js
+++ app/plugins/contentreference/nodeTree/binders.js
@@ -5,10 +5,9 @@ define(['underscore', 'jquery'], function (_, $) {
         return {
             'filter-by-allowed-types' :  function(el, model) {
                 var allowedTypes = this.model.view.model.get('allowedTypes'),
-                    inSetup = this.model.model.get('inSetup'),
                     thisModelsType = model.get('type');
 
-                if (!_.isEmpty(allowedTypes) && !_.contains(allowedTypes, thisModelsType) || inSetup) {
+                if (!_.isEmpty(allowedTypes) && !_.contains(allowedTypes, thisModelsType)) {
                     $(el).hide();
                 }
             }
