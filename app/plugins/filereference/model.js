fac767e70d6efede55c3b13f03536b4cb9361b98
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-05 16:34:18 -0800

    Created file reference plugin.

---
+++ app/plugins/filereference/model.js
@@ -0,0 +1,38 @@
+define(['grasshopperModel', 'resources', 'backbone', 'constants', 'grasshopperCollection', 'masseuse'],
+    function (Model, resources, Backbone, constants, grasshopperCollection, masseuse) {
+
+    'use strict';
+
+    var ComputedProperty = masseuse.ComputedProperty;
+
+    return Model.extend({
+        initialize : initialize,
+        defaults : {
+            resources : resources,
+            showTree : false,
+            inSetup : true,
+            selectedContentHref : new ComputedProperty(['value'], function(contentId) {
+                return constants.internalRoutes.contentDetail.replace(':id', contentId);
+            }),
+            _id : '0'
+        },
+        urlRoot : constants.api.node.url
+    });
+
+    function initialize() {
+        var self = this;
+        Model.prototype.initialize.apply(this, arguments);
+        this.set('children', new (grasshopperCollection.extend({
+            url : function() {
+                return constants.api.nodesChildren.url.replace(':id', self.get('_id'));
+            }
+        }))());
+
+        this.on('change:options', function() {
+            if (this.get('options.defaultNode') !== '0') {
+                this.set('selectedNodeLabel',
+                    this.get('children').findWhere( { _id : this.get('options.defaultNode') }).get('label'));
+            }
+        });
+    }
+});
