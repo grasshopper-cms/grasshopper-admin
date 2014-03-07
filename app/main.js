f904583640d2499fa68f56237776dd86badd48aa
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-07 11:50:16 -0800

    Alphabetize the views in main.

--- app/main.js
+++ app/main.js
@@ -84,65 +84,37 @@ require.config({
         // Views
         grasshopperBaseView : 'views/grasshopperBaseView',
 
-        loginView : 'views/login/loginView',
-        loginViewConfig : 'views/login/loginViewConfig',
-        loginViewModel : 'views/login/loginViewModel',
+        addAssetsView : 'views/addAssets/addAssetsView',
+        addAssetsViewConfig : 'views/addAssets/addAssetsViewConfig',
+        addAssetsViewModel : 'views/addAssets/addAssetsViewModel',
 
-        dashboardView : 'views/dashboard/dashboardView',
-        dashboardViewConfig : 'views/dashboard/dashboardViewConfig',
-        dashboardViewModel : 'views/dashboard/dashboardViewModel',
+        addContentView : 'views/addContent/addContentView',
+        addContentViewConfig : 'views/addContent/addContentViewConfig',
+        addContentViewModel : 'views/addContent/addContentViewModel',
 
-        headerView : 'views/header/headerView',
-        headerViewConfig : 'views/header/headerViewConfig',
-        headerViewModel : 'views/header/headerViewModel',
-
-        alertBoxView : 'views/alertBox/alertBoxView',
-        alertBoxViewConfig : 'views/alertBox/alertBoxViewConfig',
-        alertBoxViewModel : 'views/alertBox/alertBoxViewModel',
-
-        modalView : 'views/modal/modalView',
-        modalViewConfig : 'views/modal/modalViewConfig',
-        modalViewModel : 'views/modal/modalViewModel',
-
-        userDetailView : 'views/userDetail/userDetailView',
-        userDetailViewConfig : 'views/userDetail/userDetailViewConfig',
-        userDetailViewModel : 'views/userDetail/userDetailViewModel',
-
-        usersIndexView : 'views/usersIndex/usersIndexView',
-        usersIndexViewConfig : 'views/usersIndex/usersIndexViewConfig',
-        usersIndexViewModel : 'views/usersIndex/usersIndexViewModel',
+        addFolderView : 'views/addFolder/addFolderView',
+        addFolderViewConfig : 'views/addFolder/addFolderViewConfig',
+        addFolderViewModel : 'views/addFolder/addFolderViewModel',
 
         addUserView : 'views/addUser/addUserView',
         addUserViewConfig : 'views/addUser/addUserViewConfig',
         addUserViewModel : 'views/addUser/addUserViewModel',
 
-        contentBrowseView : 'views/contentBrowse/contentBrowseView',
-        contentBrowseViewConfig : 'views/contentBrowse/contentBrowseViewConfig',
-        contentBrowseViewModel : 'views/contentBrowse/contentBrowseViewModel',
+        alertBoxView : 'views/alertBox/alertBoxView',
+        alertBoxViewConfig : 'views/alertBox/alertBoxViewConfig',
+        alertBoxViewModel : 'views/alertBox/alertBoxViewModel',
 
-        contentTypeIndexView : 'views/contentTypeIndex/contentTypeIndexView',
-        contentTypeIndexViewConfig : 'views/contentTypeIndex/contentTypeIndexViewConfig',
-        contentTypeIndexViewModel : 'views/contentTypeIndex/contentTypeIndexViewModel',
-
-        contentTypeDetailView : 'views/contentTypeDetail/contentTypeDetailView',
-        contentTypeDetailViewConfig : 'views/contentTypeDetail/contentTypeDetailViewConfig',
-        contentTypeDetailViewModel : 'views/contentTypeDetail/contentTypeDetailViewModel',
-
-        mastheadView : 'views/masthead/mastheadView',
-        mastheadViewConfig : 'views/masthead/mastheadViewConfig',
-        mastheadViewModel : 'views/masthead/mastheadViewModel',
-
-        nodeDetailView : 'views/nodeDetail/nodeDetailView',
-        nodeDetailViewConfig : 'views/nodeDetail/nodeDetailViewConfig',
-        nodeDetailViewModel : 'views/nodeDetail/nodeDetailViewModel',
-
-        nodeIndexView : 'views/nodeIndex/nodeIndexView',
-        nodeIndexViewConfig : 'views/nodeIndex/nodeIndexViewConfig',
-        nodeIndexViewModel : 'views/nodeIndex/nodeIndexViewModel',
+        assetDetailView : 'views/assetDetail/assetDetailView',
+        assetDetailViewConfig : 'views/assetDetail/assetDetailViewConfig',
+        assetDetailViewModel : 'views/assetDetail/assetDetailViewModel',
 
         assetIndexView : 'views/assetIndex/assetIndexView',
         assetIndexViewConfig : 'views/assetIndex/assetIndexViewConfig',
         assetIndexViewModel : 'views/assetIndex/assetIndexViewModel',
+
+        contentBrowseView : 'views/contentBrowse/contentBrowseView',
+        contentBrowseViewConfig : 'views/contentBrowse/contentBrowseViewConfig',
+        contentBrowseViewModel : 'views/contentBrowse/contentBrowseViewModel',
 
         contentDetailView : 'views/contentDetail/contentDetailView',
         contentDetailViewConfig : 'views/contentDetail/contentDetailViewConfig',
@@ -152,25 +124,53 @@ require.config({
         contentIndexViewConfig : 'views/contentIndex/contentIndexViewConfig',
         contentIndexViewModel : 'views/contentIndex/contentIndexViewModel',
 
-        assetDetailView : 'views/assetDetail/assetDetailView',
-        assetDetailViewConfig : 'views/assetDetail/assetDetailViewConfig',
-        assetDetailViewModel : 'views/assetDetail/assetDetailViewModel',
+        contentTypeDetailView : 'views/contentTypeDetail/contentTypeDetailView',
+        contentTypeDetailViewConfig : 'views/contentTypeDetail/contentTypeDetailViewConfig',
+        contentTypeDetailViewModel : 'views/contentTypeDetail/contentTypeDetailViewModel',
 
-        addFolderView : 'views/addFolder/addFolderView',
-        addFolderViewConfig : 'views/addFolder/addFolderViewConfig',
-        addFolderViewModel : 'views/addFolder/addFolderViewModel',
+        contentTypeIndexView : 'views/contentTypeIndex/contentTypeIndexView',
+        contentTypeIndexViewConfig : 'views/contentTypeIndex/contentTypeIndexViewConfig',
+        contentTypeIndexViewModel : 'views/contentTypeIndex/contentTypeIndexViewModel',
 
-        addContentView : 'views/addContent/addContentView',
-        addContentViewConfig : 'views/addContent/addContentViewConfig',
-        addContentViewModel : 'views/addContent/addContentViewModel',
+        dashboardView : 'views/dashboard/dashboardView',
+        dashboardViewConfig : 'views/dashboard/dashboardViewConfig',
+        dashboardViewModel : 'views/dashboard/dashboardViewModel',
 
-        addAssetsView : 'views/addAssets/addAssetsView',
-        addAssetsViewConfig : 'views/addAssets/addAssetsViewConfig',
-        addAssetsViewModel : 'views/addAssets/addAssetsViewModel',
+        headerView : 'views/header/headerView',
+        headerViewConfig : 'views/header/headerViewConfig',
+        headerViewModel : 'views/header/headerViewModel',
+
+        loginView : 'views/login/loginView',
+        loginViewConfig : 'views/login/loginViewConfig',
+        loginViewModel : 'views/login/loginViewModel',
+
+        mastheadView : 'views/masthead/mastheadView',
+        mastheadViewConfig : 'views/masthead/mastheadViewConfig',
+        mastheadViewModel : 'views/masthead/mastheadViewModel',
+
+        modalView : 'views/modal/modalView',
+        modalViewConfig : 'views/modal/modalViewConfig',
+        modalViewModel : 'views/modal/modalViewModel',
+
+        nodeDetailView : 'views/nodeDetail/nodeDetailView',
+        nodeDetailViewConfig : 'views/nodeDetail/nodeDetailViewConfig',
+        nodeDetailViewModel : 'views/nodeDetail/nodeDetailViewModel',
+
+        nodeIndexView : 'views/nodeIndex/nodeIndexView',
+        nodeIndexViewConfig : 'views/nodeIndex/nodeIndexViewConfig',
+        nodeIndexViewModel : 'views/nodeIndex/nodeIndexViewModel',
 
         pluginWrapperView : 'views/pluginWrapper/pluginWrapperView',
         pluginWrapperViewConfig : 'views/pluginWrapper/pluginWrapperViewConfig',
         pluginWrapperViewModel : 'views/pluginWrapper/pluginWrapperViewModel',
+
+        userDetailView : 'views/userDetail/userDetailView',
+        userDetailViewConfig : 'views/userDetail/userDetailViewConfig',
+        userDetailViewModel : 'views/userDetail/userDetailViewModel',
+
+        usersIndexView : 'views/usersIndex/usersIndexView',
+        usersIndexViewConfig : 'views/usersIndex/usersIndexViewConfig',
+        usersIndexViewModel : 'views/usersIndex/usersIndexViewModel',
 
         // Models
         selfValidatingModel : 'models/selfValidatingModel',
@@ -208,7 +208,7 @@ require.config({
         formatters : 'formatters',
         contentTypeDetailFormatters : 'views/contentTypeDetail/contentTypeDetailFormatters',
 
-        // Resources File
+        // Resources Files
         resources : 'resources',
         constants : 'constants'
     }
