2ee0bd68640b0a32fafb5e5dee95a6916c36b508
kaijarayne <kaija.rayne@thinksolid.com>
2014-03-10 15:57:11 -0700

    checkbox can represent itself on the content type

--- app/plugins/checkbox/view.js
+++ app/plugins/checkbox/view.js
@@ -4,6 +4,34 @@ define(['grasshopperBaseView'],
         'use strict';
 
         return GrasshopperBaseView.extend({
+            beforeRender : beforeRender,
+            addOption : addOption,
+            removeOption : removeOption,
+            reduceCollection : reduceCollection
         });
+
+        function beforeRender() {
+            this.collection.reset(this.model.get('options'));
+        }
+
+        function addOption() {
+            this.collection.add({ _id: '', label: ''});
+            this.reduceCollection();
+        }
+
+        function removeOption(evt, context) {
+            this.collection.remove(context.option);
+            this.reduceCollection();
+        }
+
+        function reduceCollection() {
+            var collection = [];
+
+            this.collection.each(function(model) {
+                var option = model.toJSON();
+                collection.push(option);
+            });
+            this.model.set('options', collection);
+        }
 
     });
