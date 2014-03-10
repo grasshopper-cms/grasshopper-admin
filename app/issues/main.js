d6b323bc5694601455dd6c796ef48d3d0ca3dd94
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-10 01:49:42 -0700

    Adding Github issues support.

---
+++ app/issues/main.js
@@ -0,0 +1,35 @@
+/*global define*/
+define(['./issues/github/github', 'jquery'], function(Github, $) {
+    'use strict';
+
+    return {
+        getIssues : getIssues,
+        createIssue : createIssue
+    };
+
+    function getIssues() {
+        return true;
+    }
+
+    function createIssue(title, body, assignee, milestone, labels) {
+        var $deferred = new $.Deferred(),
+            payload = {
+                title : title,
+                body : body,
+                assignee : assignee,
+                milestone : milestone,
+                labels : labels
+            };
+
+        Github.createIssue.call(this, payload)
+            .done(function(response) {
+                $deferred.resolve(response);
+            })
+            .fail(function(error) {
+                $deferred.reject(error);
+            });
+
+        return $deferred.promise();
+    }
+
+});
