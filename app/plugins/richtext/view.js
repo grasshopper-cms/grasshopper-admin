a2c92402113d47700b9ee33b21cc9d071e8a56fd
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-11 16:30:46 -0700

    Rich Text plugin can represent itself on the content type detail view.

--- app/plugins/richtext/view.js
+++ app/plugins/richtext/view.js
@@ -1,9 +1,49 @@
 /*global define:false*/
-define(['grasshopperBaseView'],
-    function (GrasshopperBaseView) {
+define(['grasshopperBaseView', 'underscore', 'jquery'],
+    function (GrasshopperBaseView, _, $) {
+
         'use strict';
 
         return GrasshopperBaseView.extend({
+            afterRender : afterRender
         });
+
+        function afterRender() {
+            _startCkeditor.call(this)
+                .done(
+                    _setEditorValue.call(this),
+                    _setEditorEventHandling.call(this)
+                );
+        }
+
+        function _startCkeditor() {
+            var $deferred = new $.Deferred();
+
+            this.ckeditor = this.$('#ckeditor').ckeditor(
+                {
+                    customConfig : '',
+                    filebrowserBrowseUrl : '/#filebrowser/',
+                    skin : 'moono'
+                },
+                $deferred.resolve
+            ).editor;
+
+            return $deferred.promise();
+        }
+
+        function _setEditorValue() {
+            if(!_.isUndefined(this.model.get('value'))) {
+                this.ckeditor.setData(this.model.get('value'));
+            }
+        }
+
+        function _setEditorEventHandling() {
+            this.ckeditor.on('blur', _setContentValue.bind(this));
+        }
+
+        function _setContentValue() {
+            this.model.set('value', this.ckeditor.getData());
+        }
+
 
     });
