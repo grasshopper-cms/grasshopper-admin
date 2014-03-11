d195dd8be81e521c5722b58dabaaa9c779ace49b
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-11 14:33:51 -0700

    Adding ckeditor as dependency. Created richtext plugin directory.

---
+++ app/plugins/richtext/config.js
@@ -0,0 +1,26 @@
+/*global define:false*/
+define(['text!plugins/richtext/template.html', 'plugins/richtext/model', 'text!plugins/richtext/setupTemplate.html'],
+    function (richTextPluginTemplate, richTextPluginModel, setupTemplate) {
+        'use strict';
+
+        return {
+            name : 'richTextPlugin',
+            ModelType : richTextPluginModel,
+            modelData : {
+                min : 1,
+                max : 1,
+                options : false,
+                label : '',
+                type : 'richtext',
+                required : false,
+                validation : false,
+                value : ''
+            },
+            template : richTextPluginTemplate,
+            setupTemplate : setupTemplate,
+            events : {},
+            wrapper: false,
+            listeners : [],
+            mastheadButtons : []
+        };
+    });
