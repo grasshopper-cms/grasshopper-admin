8e77fcf324f1eedc87988a4000747fe20bf0ca2f
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-14 17:09:09 -0700

    This is not an oldy.

--- initConfig/rev.js
+++ initConfig/rev.js
@@ -1,11 +1,21 @@
-module.exports = {
-    dist: {
-        files: {
-            src: [
-                'build/**/*.js',
-                'build/{,*/}*.css',
-                'build/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
-            ]
+/*globals module:true */
+module.exports = function (grunt) {
+    'use strict';
+
+    var buildDirectory = grunt.config.get('buildDirectory');
+
+    grunt.config('rev', {
+        dist: {
+            files: {
+                src: [
+                    buildDirectory + '/**/*.js',
+                    buildDirectory + '/{,*/}*.css',
+                    buildDirectory + '/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                ]
+            }
         }
-    }
+    });
+
+    grunt.loadNpmTasks('grunt-rev');
+
 };
