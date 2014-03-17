d6c624578620594e73308ead8491231dd3d70cde
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-17 13:55:12 -0700

    Adding all possible themes and language modes to the code editor.

--- app/plugins/codeeditor/model.js
+++ app/plugins/codeeditor/model.js
@@ -1,25 +1,59 @@
-define(['grasshopperModel', 'resources'], function (Model, resources) {
+define(['grasshopperModel', 'resources', 'masseuse'], function (Model, resources, masseuse) {
+
     'use strict';
+
+    var ComputedProperty = masseuse.ComputedProperty;
 
     return Model.extend({
         defaults : {
             resources : resources,
             defaultTheme : 'ace/theme/monokai',
             defaultMode : 'ace/mode/javascript',
-            possibleThemes : [
-                'red',
-                'blue',
-                'green',
-                'black',
-                'purple'
+            possibleLanguages : [ // these live ace/mode/<name underscore>
+                'javascript',
+                'html',
+                'css'
             ],
-            possibleLanguages : [
-                'javs',
-                'bluesld',
-                'lkdjhlkjd',
-                'euheuhe',
-                'lkjss'
-            ]
+            possibleThemes : [ // these live ace/theme/<name underscore>
+                    'ambiance',
+                    'chaos',
+                    'chrome',
+                    'clouds',
+                    'clouds midnight',
+                    'cobalt',
+                    'crimson editor',
+                    'dawn',
+                    'dreamweaver',
+                    'eclipse',
+                    'github',
+                    'idle fingers',
+                    'katzenmilch',
+                    'kr theme',
+                    'kuroir',
+                    'merbivore',
+                    'merbivore soft',
+                    'mono industrial',
+                    'monokai',
+                    'pastel on dark',
+                    'solarized dark',
+                    'solarized light',
+                    'terminal',
+                    'textmate',
+                    'tomorrow',
+                    'tomorrow night',
+                    'tomorrow night blue',
+                    'tomorrow night bright',
+                    'tomorrow night eighties',
+                    'twilight',
+                    'vibrant ink',
+                    'xcode'
+            ],
+            currentThemeLocation : new ComputedProperty(['options.theme'], function(themeName) {
+                return 'ace/theme/' + themeName.replace(' ', '_');
+            }),
+            currentModeLocation : new ComputedProperty(['options.language'], function(languageName) {
+                return 'ace/mode/' + languageName.replace(' ', '_');
+            })
         }
     });
 
