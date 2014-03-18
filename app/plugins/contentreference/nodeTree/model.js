7f7034027333cba55c19c84b72426daab53bcc55
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-18 13:54:17 -0700

    content reference binder has been transitioned to the jquery ui accordion.

--- app/plugins/contentreference/nodeTree/model.js
+++ app/plugins/contentreference/nodeTree/model.js
@@ -1,19 +1,13 @@
-define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants', 'masseuse'],
-    function (Model, resources, grasshopperCollection, constants, masseuse) {
+define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants'],
+    function (Model, resources, grasshopperCollection, constants) {
     'use strict';
-
-    var ComputedProperty = masseuse.ComputedProperty;
 
     return Model.extend({
         initialize : initialize,
         defaults : {
             resources : resources,
             loading : false,
-            hasFetchedContent : false,
-            folderOpen : false,
-            folderClass : new ComputedProperty(['folderOpen'], function(folderOpen) {
-                return folderOpen ? 'icon-folder-open' : 'icon-folder-close' ;
-            })
+            hasFetchedContent : false
         }
     });
 
