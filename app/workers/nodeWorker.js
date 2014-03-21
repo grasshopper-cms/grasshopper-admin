c0cf1824194c69cedea39611d06a86164f2d4e72
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-21 14:37:12 -0700

    Actions buttons now work.

--- app/workers/nodeWorker.js
+++ app/workers/nodeWorker.js
@@ -1,5 +1,5 @@
-define(['api', 'jquery'],
-    function (Api, $) {
+define(['api', 'jquery', 'resources', 'contentTypeWorker', 'underscore', 'constants'],
+    function (Api, $, resources, contentTypeWorker, _, constants) {
         'use strict';
 
         return {
@@ -19,165 +19,158 @@ define(['api', 'jquery'],
             return $deferred.promise();
         }
 
-        function editName(nodeId) {
-            console.log('edit name');
-            console.log(nodeId);
+        function editName() {
+            var self = this;
+
+            $.when(_askUserForNewNodeName.call(this), this.model.fetch())
+                .then(function(modalData) {
+                    self.model.set('label', modalData.data);
+                    _saveThisNode.call(self);
+                });
         }
 
-        function editContentTypes(nodeId) {
-            console.log('edit allowed types');
-            console.log(nodeId);
+        function editContentTypes() {
+            this.model.fetch()
+                .then(_getAvailableContentTypes.bind(this))
+                .then(_askUserWhichContentTypesToAttach.bind(this))
+                .then(_attachContentTypesToNode.bind(this));
         }
 
-        function deleteNode(nodeId) {
-            console.log('deleteNode');
-            console.log(nodeId);
+        function deleteNode() {
+            _warnUserBeforeDeleting.call(this)
+                .done(_actuallyDeleteNode.bind(this));
         }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        function _askUserForNewNodeName() {
+            return this.displayModal(
+                {
+                    msg : resources.node.editName,
+                    type : 'input',
+                    data : this.model.get('label')
+                });
+        }
+
+        function _saveThisNode() {
+            var $deferred = new $.Deferred();
+
+            this.model.save()
+                .done(_handleSuccessfulNodeSave.bind(this, $deferred))
+                .fail(_handleFailedNodeSave.bind(this, $deferred));
+
+            return $deferred.promise();
+        }
+
+        function _handleSuccessfulNodeSave($deferred) {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.node.successfullyUpdated,
+                    status : true
+                }
+            );
+            $deferred.resolve();
+        }
+
+        function _handleFailedNodeSave($deferred) {
+            this.displayAlertBox(
+                {
+                    msg : resources.node.errorUpdated
+                }
+            );
+            $deferred.reject();
+        }
+
+        function _getAvailableContentTypes() {
+            return contentTypeWorker.getAvailableContentTypes(this.model.get('allowedTypes'));
+        }
+
+        function _askUserWhichContentTypesToAttach(availableContentTypes) {
+            return this.displayModal(
+                {
+                    msg : resources.contentType.editContentTypes,
+                    type : 'checkbox',
+                    data : availableContentTypes
+                });
+        }
+
+        function _attachContentTypesToNode(modalData) {
+            var selectedContentTypes = _.where(modalData.data, {checked: true}),
+                contentTypeToPost = _.map(selectedContentTypes, function(type) {
+                    return _.pick(type, 'label', 'helpText', '_id');
+                });
+
+            this.model.set('allowedTypes', contentTypeToPost);
+
+            this.model.save()
+                .done(_handleSuccessfulContentTypeAddition.bind(this))
+                .fail(_handleFailedContentTypeAddition.bind(this));
+        }
+
+        function _handleSuccessfulContentTypeAddition() {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.contentType.contentTypeAdded,
+                    status : true
+                }
+            );
+        }
+
+        function _handleFailedContentTypeAddition(msg) {
+            this.displayAlertBox(
+                {
+                    msg : msg
+                }
+            );
+        }
+
+        function _warnUserBeforeDeleting() {
+            return this.displayModal(
+                {
+                    msg : resources.node.deletionWarning
+                });
+        }
+
+        function _actuallyDeleteNode() {
+            var self = this;
+            this.model.fetch()
+                .then(function() {
+                    self.model.destroy()
+                        .done(_handleSuccessfulNodeDeletion.bind(self))
+                        .fail(_handleFailedNodeDeletion.bind(self));
+                });
+        }
+
+        function _handleSuccessfulNodeDeletion() {
+            this.displayTemporaryAlertBox(
+                {
+                    msg : resources.node.successfullyDeletedPre + this.model.get('label') +
+                        resources.node.successfullyDeletedPost,
+                    status : true
+                }
+            );
+            this.remove();
+
+            _redirectToParent.call(this);
+        }
+
+        function _handleFailedNodeDeletion() {
+            this.displayAlertBox(
+                {
+                    msg : resources.node.errorDeleted + this.model.get('label')
+                }
+            );
+        }
+
+        function _redirectToParent() {
+            var parent = this.model.get('parent');
+
+            if(parent && parent._id) {
+                this.app.router.navigateTrigger(constants.internalRoutes.nodeDetail.replace(':id', parent._id));
+            } else {
+                this.app.router.navigateTrigger(constants.internalRoutes.content);
+            }
+
+        }
+
     });
-
-//        function afterRender() {
-//            if(this.model.isNew()) {
-//                _saveNodeWorkflow.call(this);
-//            }
-//        }
-//
-//        function prepareToDeleteNode () {
-//            this.displayModal(
-//                {
-//                    msg : resources.node.deletionWarning
-//                })
-//                .then(_deleteNode.bind(this));
-//        }
-//
-//        function handleRowClick () {
-//            this.app.router.navigateTrigger(this.model.get('href'));
-//            return false;
-//        }
-//
-//        function prepareToEditNode () {
-//            var self = this;
-//
-//            $.when(_askUserForNewNodeName.call(this), this.model.fetch())
-//                .then(function(modalData) {
-//                    self.model.set('label', modalData.data);
-//                    _saveNodeWorkflow.call(self);
-//                });
-//        }
-//
-//        function _askUserForNewNodeName() {
-//            return this.displayModal(
-//                {
-//                    msg : resources.node.editName,
-//                    type : 'input',
-//                    data : this.model.get('label')
-//                });
-//        }
-//
-//        function editAllowedTypes() {
-//            _saveNodeWorkflow.call(this);
-//        }
-//
-//        function _saveNodeWorkflow() {
-//            $
-//                .when(_getAvailableContentTypes.call(this), _saveThisNode.call(this))
-//                .then(_askUserWhichContentTypesToAttach.bind(this))
-//                .then(_attachContentTypesToNode.bind(this));
-//        }
-//
-//        function _saveThisNode() {
-//            var $deferred = new $.Deferred();
-//
-//            this.model.save()
-//                .done(_handleSuccessfulNodeSave.bind(this, $deferred))
-//                .fail(_handleFailedNodeSave.bind(this, $deferred));
-//
-//            return $deferred.promise();
-//        }
-//
-//        function _attachContentTypesToNode(modalData) {
-//            var selectedContentTypes = _.where(modalData.data, {checked: true}),
-//                contentTypeToPost = _.map(selectedContentTypes, function(type) {
-//                    return _.pick(type, 'label', 'helpText', '_id');
-//                });
-//
-//            this.model.set('allowedTypes', contentTypeToPost);
-//
-//            this.model.save()
-//                .done(_handleSuccessfulContentTypeAddition.bind(this))
-//                .fail(_handleFailedContentTypeAddition.bind(this));
-//        }
-//
-//        function _handleFailedContentTypeAddition(msg) {
-//            this.displayAlertBox(
-//                {
-//                    msg : msg
-//                }
-//            );
-//        }
-//
-//        function _handleSuccessfulContentTypeAddition() {
-//            this.displayTemporaryAlertBox(
-//                {
-//                    msg : resources.contentType.contentTypeAdded,
-//                    status : true
-//                }
-//            );
-//        }
-//
-//        function _handleFailedNodeSave($deferred) {
-//            this.displayAlertBox(
-//                {
-//                    msg : resources.node.errorUpdated
-//                }
-//            );
-//            $deferred.reject();
-//        }
-//
-//        function _handleSuccessfulNodeSave($deferred) {
-//            this.displayTemporaryAlertBox(
-//                {
-//                    msg : resources.node.successfullyUpdated,
-//                    status : true
-//                }
-//            );
-//            $deferred.resolve();
-//        }
-//
-//        function _askUserWhichContentTypesToAttach(availableContentTypes) {
-//            return this.displayModal(
-//                {
-//                    msg : resources.contentType.editContentTypes,
-//                    type : 'checkbox',
-//                    data : availableContentTypes
-//                });
-//        }
-//
-//        function _getAvailableContentTypes() {
-//            return contentTypeWorker.getAvailableContentTypes(this.model.get('allowedTypes'));
-//        }
-//
-//        function _deleteNode() {
-//            this.model.destroy()
-//                .done(_handleSuccessfulNodeDeletion.bind(this))
-//                .fail(_handleFailedNodeDeletion.bind(this));
-//        }
-//
-//        function _handleSuccessfulNodeDeletion() {
-//            this.displayTemporaryAlertBox(
-//                {
-//                    msg : resources.node.successfullyDeletedPre + this.model.get('label') +
-//                        resources.node.successfullyDeletedPost,
-//                    status : true
-//                }
-//            );
-//            this.remove();
-//        }
-//
-//        function _handleFailedNodeDeletion() {
-//            this.displayAlertBox(
-//                {
-//                    msg : resources.node.errorDeleted + this.model.get('label')
-//                }
-//            );
-//        }
-//    });
