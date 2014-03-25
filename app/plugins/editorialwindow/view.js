717be2188fb80f7ad321be310e86baaa7c5d58dc
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-25 14:25:32 -0700

    GRAS-983

--- app/plugins/editorialwindow/view.js
+++ app/plugins/editorialwindow/view.js
@@ -1,15 +1,24 @@
 /*global define:false*/
-define(['grasshopperBaseView'],
-    function (GrasshopperBaseView) {
+define(['grasshopperBaseView', 'moment'],
+    function (GrasshopperBaseView, moment) {
         'use strict';
 
-
         return GrasshopperBaseView.extend({
-            afterRender : afterRender
+            setValidFromToNow : setValidFromToNow,
+            setValidToToNow : setValidToToNow,
+            setValidToNeverExpire : setValidToNeverExpire
         });
 
-        function afterRender() {
+        function setValidFromToNow() {
+            this.model.set('value.validFrom', moment());
+        }
 
+        function setValidToToNow() {
+            this.model.set('value.validTo', moment());
+        }
+
+        function setValidToNeverExpire() {
+            this.model.set('value.validTo', moment('December 31 3000'));
         }
 
     });
