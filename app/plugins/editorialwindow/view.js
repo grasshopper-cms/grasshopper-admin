37e1d1662ead85285336110c0f9c73c58a28d8b7
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-24 18:22:01 -0700

    editorial window plugin progress commit.

--- app/plugins/editorialwindow/view.js
+++ app/plugins/editorialwindow/view.js
@@ -1,9 +1,32 @@
 /*global define:false*/
-define(['grasshopperBaseView'],
-    function (GrasshopperBaseView) {
+define(['grasshopperBaseView', 'underscore'],
+    function (GrasshopperBaseView, _) {
         'use strict';
 
         return GrasshopperBaseView.extend({
+            beforeRender : beforeRender
         });
+
+        function beforeRender() {
+            _splitValue.call(this);
+        }
+
+        function _splitValue() {
+            if(!_.isUndefined(this.model.get('value'))) {
+                this.model.set('validTo', this.model.get('value.validTo'));
+                this.model.set('validFrom', this.model.get('value.validFrom'));
+            } else {
+                _setToTodaysDate.call(this);
+            }
+        }
+
+        function _setToTodaysDate() {
+            var today = new Date();
+
+            this.model.set({
+                validFrom: today,
+                validTo: today
+            });
+        }
 
     });
