60e1bbf55bf55b652cd61e8cfb0a81d9a7c4fefd
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-03-24 18:22:01 -0700

    editorial window plugin progress commit.

--- app/plugins/editorialwindow/model.js
+++ app/plugins/editorialwindow/model.js
@@ -2,9 +2,21 @@ define(['grasshopperModel', 'resources'], function (Model, resources) {
     'use strict';
 
     return Model.extend({
+        initialize : initialize,
         defaults : {
-            resources : resources
+            resources : resources,
+            validFrom : '',
+            validTo : ''
         }
     });
+
+    function initialize() {
+        this.on('change:validTo', function() {
+            console.log(arguments);
+        });
+        this.on('change:validFrom', function() {
+            console.log(arguments);
+        });
+    }
 
 });
