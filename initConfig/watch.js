15fb265a0f1c84afa8b9d915d499e40f65bab3b1
kaijarayne <kaija.rayne@thinksolid.com>
2014-03-28 11:55:52 -0700

    GRAS-1027 updating watch.js file

--- initConfig/watch.js
+++ initConfig/watch.js
@@ -6,47 +6,47 @@ module.exports = function (grunt) {
         options : {
             // Start a live reload server on the default port: 35729
             livereload : false
-//            nospawn: true
+            nospawn: true
         },
-//        build : {
-//            options : {
-//                // Start a live reload server on the default port: 35729
-//                livereload : true
-//            },
-//            files : [
-//                'build/**',
-//                '!build/vendor/**'
-//            ]
-//        },
-//        dev : {
-//            options : {
-//                // Start a live reload server on the default port: 35729
-//                livereload : true
-//            },
-//            files : [
-//                'app/**',
-//                '!app/**/*.html',
-//                '!app/**/*.scss',// Exclusion order is relevant. Exclude Sass files.
-//                '!app/vendor/**'
-//            ],
-//            tasks : [
-//                'jshint',
-//                'setupBowerCopy',
-//                'copy:build',
-//                'registerPlugins',
-//                'paths',
-//                'setBuildConfig'
-//            ]
-//        },
-//        tests : {
-//            options : {
-//                // Start a live reload server on the default port: 35729
-//                livereload : true
-//            },
-//            files : [
-//                'tests/**/*.js'
-//            ]
-//        },
+        build : {
+            options : {
+                // Start a live reload server on the default port: 35729
+                livereload : true
+            },
+            files : [
+                'build/**',
+                '!build/vendor/**'
+            ]
+        },
+        dev : {
+            options : {
+                // Start a live reload server on the default port: 35729
+                livereload : true
+            },
+            files : [
+                'app/**',
+                '!app/**/*.html',
+                '!app/**/*.scss',// Exclusion order is relevant. Exclude Sass files.
+                '!app/vendor/**'
+            ],
+            tasks : [
+                'jshint',
+                'setupBowerCopy',
+                'copy:build',
+                'registerPlugins',
+                'paths',
+                'setBuildConfig'
+            ]
+        },
+        tests : {
+            options : {
+                // Start a live reload server on the default port: 35729
+                livereload : true
+            },
+            files : [
+                'tests/**/*.js'
+            ]
+        },
         sass : {
             options : {
                 livereload : true
