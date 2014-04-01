ae623360dcd9f86316d314f554ffb91db05eb983
kaijarayne <kaija.rayne@thinksolid.com>
2014-03-31 17:13:37 -0700

    GRAS-18 Validation for fields, progress commit

--- app/validation/views/alpha/view.js
+++ app/validation/views/alpha/view.js
@@ -1,6 +1,32 @@
-define([], function () {
-    'use strict';
-    return {
+/*global define:false*/
+define(['grasshopperBaseView', 'validationAlphaConfig'],
+    function (GrasshopperBaseView, validationAlphaConfig) {
+        'use strict';
 
-    };
-});
+        return GrasshopperBaseView.extend({
+            defaultOptions : validationAlphaConfig,
+            afterRender : afterRender
+        });
+
+        function afterRender() {
+            _initializeSortableAccordions.call(this);
+        }
+
+        function _initializeSortableAccordions() {
+            var $accordion = this.$el;
+
+            $accordion
+                .accordion(
+                {
+                    header : '.accordionHeader',
+                    icons : false,
+                    active : false,
+                    collapsible : true,
+                    heightStyle : 'content'
+                }
+            );
+        }
+
+    });
+
+
