bda710881cd67bca5cd6a79ec3ab9bec6d84fb46
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-02 16:53:06 -0700

    GRAS-1106 Date validation view functioning

--- app/validation/views/date/view.js
+++ app/validation/views/date/view.js
@@ -1,11 +1,12 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationDateConfig'],
-    function (GrasshopperBaseView, validationDateConfig) {
+define(['grasshopperBaseView', 'validationDateConfig', 'resources'],
+    function (GrasshopperBaseView, validationDateConfig, resources) {
         'use strict';
 
         return GrasshopperBaseView.extend({
             defaultOptions : validationDateConfig,
-            afterRender : afterRender
+            afterRender : afterRender,
+            deleteThisValidation : deleteThisValidation
         });
 
         function afterRender() {
@@ -18,7 +19,7 @@ define(['grasshopperBaseView', 'validationDateConfig'],
             $accordion
                 .accordion(
                 {
-                    header : '.accordionHeader',
+                    header : '.validationAccordion',
                     icons : false,
                     active : false,
                     collapsible : true,
@@ -27,6 +28,22 @@ define(['grasshopperBaseView', 'validationDateConfig'],
             );
         }
 
+        function deleteThisValidation(e) {
+            e.stopPropagation();
+            _warnUserBeforeDeleting.call(this)
+                .done(_actuallyDeleteThisValidation.bind(this));
+        }
+
+        function _warnUserBeforeDeleting() {
+            return this.displayModal({
+                header : resources.warning,
+                msg : resources.validationViews.deletionWarning
+            });
+        }
+
+        function _actuallyDeleteThisValidation() {
+            this.parent.model.get('validationCollection').remove(this.model);
+            this.remove();
+        }
+
     });
-
-
