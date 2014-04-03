5f699ce9d22bf13331dc969996cb07b95264b181
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-04-02 17:04:53 -0700

    Adding the Time validation view.

--- app/validation/views/time/view.js
+++ app/validation/views/time/view.js
@@ -1,11 +1,12 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationTimeConfig'],
-    function (GrasshopperBaseView, validationTimeConfig) {
+define(['grasshopperBaseView', 'validationTimeConfig', 'resources'],
+    function (GrasshopperBaseView, validationTimeConfig, resources) {
         'use strict';
 
         return GrasshopperBaseView.extend({
             defaultOptions : validationTimeConfig,
-            afterRender : afterRender
+            afterRender : afterRender,
+            deleteThisValidation : deleteThisValidation
         });
 
         function afterRender() {
@@ -18,7 +19,7 @@ define(['grasshopperBaseView', 'validationTimeConfig'],
             $accordion
                 .accordion(
                 {
-                    header : '.accordionHeader',
+                    header : '.validationAccordion',
                     icons : false,
                     active : false,
                     collapsible : true,
@@ -27,5 +28,22 @@ define(['grasshopperBaseView', 'validationTimeConfig'],
             );
         }
 
-    });
+        function deleteThisValidation(e) {
+            e.stopPropagation();
+            _warnUserBeforeDeleting.call(this)
+                .done(_actuallyDeleteThisValidation.bind(this));
+        }
 
+        function _warnUserBeforeDeleting() {
+            return this.displayModal({
+                header : resources.warning,
+                msg : resources.validationViews.deletionWarning
+            });
+        }
+
+        function _actuallyDeleteThisValidation() {
+            this.parent.model.get('validationCollection').remove(this.model);
+            this.remove();
+        }
+
+    });
