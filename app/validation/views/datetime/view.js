cb3dfa36af08cf0a39a7cb09da30f6b829292a2a
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-03 12:12:39 -0700

    GRAS-1124 refactoring datetime validation view

--- app/validation/views/datetime/view.js
+++ app/validation/views/datetime/view.js
@@ -1,49 +1,9 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationDatetimeConfig', 'resources'],
-    function (GrasshopperBaseView, validationDatetimeConfig, resources) {
+define(['grasshopperBaseView', 'validationDatetimeConfig'],
+    function (GrasshopperBaseView, validationDatetimeConfig) {
         'use strict';
 
         return GrasshopperBaseView.extend({
-            defaultOptions : validationDatetimeConfig,
-            afterRender : afterRender,
-            deleteThisValidation : deleteThisValidation
+            defaultOptions : validationDatetimeConfig
         });
-
-        function afterRender() {
-            _initializeSortableAccordions.call(this);
-        }
-
-        function _initializeSortableAccordions() {
-            var $accordion = this.$el;
-
-            $accordion
-                .accordion(
-                {
-                    header : '.validationAccordion',
-                    icons : false,
-                    active : false,
-                    collapsible : true,
-                    heightStyle : 'content'
-                }
-            );
-        }
-
-        function deleteThisValidation(e) {
-            e.stopPropagation();
-            _warnUserBeforeDeleting.call(this)
-                .done(_actuallyDeleteThisValidation.bind(this));
-        }
-
-        function _warnUserBeforeDeleting() {
-            return this.displayModal({
-                header : resources.warning,
-                msg : resources.validationViews.deletionWarning
-            });
-        }
-
-        function _actuallyDeleteThisValidation() {
-            this.parent.model.get('validationCollection').remove(this.model);
-            this.remove();
-        }
-
     });
