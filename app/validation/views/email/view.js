a367fe2caf464dc86a0e331cb6d2f32d99251329
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-03 12:22:52 -0700

    GRAS-1124 refactoring datetime email view

--- app/validation/views/email/view.js
+++ app/validation/views/email/view.js
@@ -1,34 +1,9 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationEmailConfig', 'resources'],
-    function (GrasshopperBaseView, validationEmailConfig, resources) {
+define(['grasshopperBaseView', 'validationEmailConfig'],
+    function (GrasshopperBaseView, validationEmailConfig) {
         'use strict';
 
         return GrasshopperBaseView.extend({
-            defaultOptions : validationEmailConfig,
-            afterRender : afterRender,
-            deleteThisValidation : deleteThisValidation
+            defaultOptions : validationEmailConfig
         });
-
-        function afterRender() {
-            this.parent.model.updateValidationRulesOnModel();
-        }
-
-        function deleteThisValidation(e) {
-            e.stopPropagation();
-            _warnUserBeforeDeleting.call(this)
-                .done(_actuallyDeleteThisValidation.bind(this));
-        }
-
-        function _warnUserBeforeDeleting() {
-            return this.displayModal({
-                header : resources.warning,
-                msg : resources.validationViews.deletionWarning
-            });
-        }
-
-        function _actuallyDeleteThisValidation() {
-            this.parent.model.get('validationCollection').remove(this.model);
-            this.remove();
-        }
-
     });
