16bb60938f2e9a39da7ef8c267f7b4b92f9c07d7
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-03 12:34:20 -0700

    GRAS-1124 refactoring regex validation view

--- app/validation/views/regex/view.js
+++ app/validation/views/regex/view.js
@@ -1,49 +1,9 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationRegexConfig', 'resources'],
-    function (GrasshopperBaseView, validationRegexConfig, resources) {
+define(['grasshopperBaseView', 'validationRegexConfig'],
+    function (GrasshopperBaseView, validationRegexConfig) {
         'use strict';
 
         return GrasshopperBaseView.extend({
-            defaultOptions : validationRegexConfig,
-            afterRender : afterRender,
-            deleteThisValidation : deleteThisValidation
+            defaultOptions : validationRegexConfig
         });
-
-        function afterRender() {
-            _initializeSortableAccordions.call(this);
-        }
-
-        function _initializeSortableAccordions() {
-            var $accordion = this.$el;
-
-            $accordion
-                .accordion(
-                {
-                    header : '.validationAccordion',
-                    icons : false,
-                    active : false,
-                    collapsible : true,
-                    heightStyle : 'content'
-                }
-            );
-        }
-
-        function deleteThisValidation(e) {
-            e.stopPropagation();
-            _warnUserBeforeDeleting.call(this)
-                .done(_actuallyDeleteThisValidation.bind(this));
-        }
-
-        function _warnUserBeforeDeleting() {
-            return this.displayModal({
-                header : resources.warning,
-                msg : resources.validationViews.deletionWarning
-            });
-        }
-
-        function _actuallyDeleteThisValidation() {
-            this.parent.model.get('validationCollection').remove(this.model);
-            this.remove();
-        }
-
     });
