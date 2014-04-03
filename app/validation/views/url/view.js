2b172a2d89b21930009c48294eb6602a624feb23
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-03 09:33:29 -0700

    GRAS-1112 Adding URL validation view

--- app/validation/views/url/view.js
+++ app/validation/views/url/view.js
@@ -1,11 +1,12 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'validationUrlConfig'],
-    function (GrasshopperBaseView, validationUrlConfig) {
+define(['grasshopperBaseView', 'validationUrlConfig', 'resources'],
+    function (GrasshopperBaseView, validationUrlConfig, resources) {
         'use strict';
 
         return GrasshopperBaseView.extend({
             defaultOptions : validationUrlConfig,
-            afterRender : afterRender
+            afterRender : afterRender,
+            deleteThisValidation : deleteThisValidation
         });
 
         function afterRender() {
@@ -18,7 +19,7 @@ define(['grasshopperBaseView', 'validationUrlConfig'],
             $accordion
                 .accordion(
                 {
-                    header : '.accordionHeader',
+                    header : '.validationAccordion',
                     icons : false,
                     active : false,
                     collapsible : true,
@@ -27,6 +28,22 @@ define(['grasshopperBaseView', 'validationUrlConfig'],
             );
         }
 
+        function deleteThisValidation(e) {
+            e.stopPropagation();
+            _warnUserBeforeDeleting.call(this)
+                .done(_actuallyDeleteThisValidation.bind(this));
+        }
+
+        function _warnUserBeforeDeleting() {
+            return this.displayModal({
+                header : resources.warning,
+                msg : resources.validationViews.deletionWarning
+            });
+        }
+
+        function _actuallyDeleteThisValidation() {
+            this.parent.model.get('validationCollection').remove(this.model);
+            this.remove();
+        }
+
     });
-
-
