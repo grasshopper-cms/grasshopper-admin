e42c2d83c1d7cff9f767646b7aca4fe9a5df031a
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-04-04 11:12:43 -0700

    Deleting all bower dependencies, adding bower install to the build task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,6 +80,7 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('build', [
+        'shell:bowerInstall',
         'setupBowerCopy',
         'copy:build',
         'copy:vendor',
