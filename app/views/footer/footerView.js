f9fad8e22736fff7ed9473c1b8c384398842dc8d
kaijarayne <kaija.rayne@thinksolid.com>
2014-04-16 16:49:07 -0700

    GRAS-1209 creating a footer view

---
+++ app/views/footer/footerView.js
@@ -0,0 +1,11 @@
+/*global define:false*/
+define(['grasshopperBaseView', 'footerViewConfig'], function (GrasshopperBaseView, footerViewConfig) {
+    'use strict';
+    return GrasshopperBaseView.extend({
+        defaultOptions : footerViewConfig,
+        beforeRender : beforeRender
+    });
+
+    function beforeRender() {
+    }
+});
