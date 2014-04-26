359bf868b896614a365f2e166e8b90c2919e4b56
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-04-25 17:17:43 -0700

    Removed the NodeDetailROW, this view is now the plain old nodeDetailView.

--- app/views/nodeDetail/nodeDetailRow/config.js
+++
@@ -1,8 +0,0 @@
-define(['nodeDetailViewConfig', 'jquery', 'text!views/nodeDetail/nodeDetailRow/template.html'],
-    function(nodeDetailViewConfig, $, template) {
-        'use strict';
-
-        return $.extend(true, {}, nodeDetailViewConfig, {
-            template : template
-        });
-    });
--- app/views/nodeDetail/nodeDetailRow/view.js
+++
@@ -1,8 +0,0 @@
-define(['nodeDetailView', 'nodeDetailRowConfig'], function(NodeDetailView, nodeDetailRowConfig) {
-    'use strict';
-
-    return NodeDetailView.extend({
-        defaultOptions : nodeDetailRowConfig
-    });
-
-});
