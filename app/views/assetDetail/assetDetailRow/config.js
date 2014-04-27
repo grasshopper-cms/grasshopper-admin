8c65998a875913c325c49faabb288f9eff69b3ee
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-04-27 09:30:20 -0700

    The Asset detail row has been deleted, the asset detail view has only one template, so I am using the asset detail view for the row.

--- app/views/assetDetail/assetDetailRow/config.js
+++
@@ -1,9 +0,0 @@
-define(['assetDetailViewConfig', 'jquery', 'text!views/assetDetail/assetDetailRow/template.html'],
-    function(assetDetailViewConfig, $, template) {
-        'use strict';
-
-        return $.extend(true, {}, assetDetailViewConfig, {
-            name : 'assetDetailRow',
-            template : template
-        });
-    });
