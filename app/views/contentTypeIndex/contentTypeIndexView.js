c4ec1130c193ec76cd9708e59cbcac89af616056
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-04-30 11:06:40 -0700

    Content Type index view now uses a child view binder.

--- app/views/contentTypeIndex/contentTypeIndexView.js
+++ app/views/contentTypeIndex/contentTypeIndexView.js
@@ -1,44 +1,17 @@
 /*global define:false*/
-define(['grasshopperBaseView', 'contentTypeIndexViewConfig', 'contentTypeDetailView',
-    'text!views/contentTypeDetail/_contentTypeDetailRow.html',
-    'underscore', 'resources', 'constants'],
-    function (GrasshopperBaseView, contentTypeIndexViewConfig, ContentTypeDetailView,
-              rowTemplate,
-              _, resources, constants) {
+define(['grasshopperBaseView', 'contentTypeIndexViewConfig', 'constants'],
+    function (GrasshopperBaseView, contentTypeIndexViewConfig, constants) {
         'use strict';
 
         return GrasshopperBaseView.extend({
             defaultOptions : contentTypeIndexViewConfig,
             beforeRender : beforeRender,
-            insertContentTypeDetailRow : insertContentTypeDetailRow,
             newContentType : newContentType
         });
 
         function beforeRender ($deferred) {
-            var self = this;
-            this.model.fetch()
-                .done(function () {
-                    _.each(self.model.get('results'), function (data) {
-                        self.insertContentTypeDetailRow(data);
-                    });
-                    $deferred.resolve();
-                })
-                .fail(function (xhr) {
-                    self.fireErrorModal(resources.contentType.serverError + ' ' + xhr.responseJSON.message);
-                });
-        }
-
-        function insertContentTypeDetailRow (data) {
-
-            var contentTypeDetailView = new ContentTypeDetailView({
-                    name : 'contentDetailrow',
-                    appendTo : '#contentTypeIndexTable',
-                    wrapper : false,
-                    template : rowTemplate,
-                    modelData : data,
-                    mastheadButtons : null
-                });
-            this.addChild(contentTypeDetailView);
+            this.model.get('contentTypes').fetch()
+                .done($deferred.resolve);
         }
 
         function newContentType() {
