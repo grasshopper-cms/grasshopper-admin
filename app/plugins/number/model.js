13acb5ff98d4bfd986140d2baba493440b66e442
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2014-07-08 14:14:22 -0700

    updates to smaller textboxes, editorial layout

---
+++ app/plugins/number/model.js
@@ -0,0 +1,12 @@
+define(['grasshopperModel', 'resources'], function (Model, resources) {
+    'use strict';
+
+
+    return Model.extend({
+        defaults : {
+            resources : resources
+
+        }
+    });
+
+});
