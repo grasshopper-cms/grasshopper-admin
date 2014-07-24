654a834d237ee2580634577b46dfc323cdf60e3b
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-07-24 12:33:30 -0700

    Build: 1 Branch: kirby  0.27.1 SHA: d536cd5

---
+++ index.js
@@ -0,0 +1,12 @@
+/* jshint node:true */
+'use strict';
+console.log('starting');
+var express = require('express'),
+    app = express(),
+    api = require('grasshopper-api'),
+    PORT = process.env.PORT || 3000;
+
+app.use(api('use proxy').router);
+app.use(express.static(__dirname + '/public'));
+console.log('litening on: ' + PORT);
+app.listen(PORT);
