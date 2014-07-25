24825d842610991c718e07b5f6bfd0c075432197
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-07-24 19:02:39 -0700

    Removing the login attribute. Changed to username.

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -7,7 +7,7 @@ define([
     return Model.extend({
         idAttribute : '_id',
         defaults : {
-            login : false,
+            username : false,
             role : false,
             enabled : false,
             email : false,
