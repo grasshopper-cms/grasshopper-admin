32ba70a6b96014370f4afa54cfcf1b551a7a5d22
Andrew Gurinovich <altmind@gmail.com>
2014-07-25 10:49:05 -0700

    GRAS-1447 New content types do not have an ID in their Title

--- app/views/contentTypeDetail/contentTypeDetailView.js
+++ app/views/contentTypeDetail/contentTypeDetailView.js
@@ -47,6 +47,7 @@ define(['grasshopperBaseView', 'contentTypeDetailViewConfig',
     function _handleNewContentType($deferred) {
         var textboxModelData = _.clone(_.findWhere(plugins.fields, { type : 'textbox' }).config.modelData);
 
+        textboxModelData._id = 'title';
         textboxModelData.label = 'Title';
         this.collection.reset(textboxModelData);
         _updateMastheadBreadcrumbs.call(this, $deferred, true);
