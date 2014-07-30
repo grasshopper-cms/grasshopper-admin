3a8fe5eb24a6cd4852d1ebaadb37afb4498d1f0b
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-07-29 18:00:53 -0700

    Build: 20 Branch: kirby  0.27.6 SHA: f792fa9

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ console.log('starting');
 var express = require('express'),
     app = express(),
     api = require('grasshopper-api'),
-    config = require('./ghapi.json'),
     PORT = process.env.PORT || 3000;
 
-api = api(config);
+api = api();
 app.use(api.ghApi || api.router);
 app.use(express.static(__dirname + '/public'));
 console.log('litening on: ' + PORT);
