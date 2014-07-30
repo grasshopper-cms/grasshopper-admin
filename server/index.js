970e3941e5f8ae6e8776845f31b2b8f157469c99
Travis McHattie <travis.mchattie@thinksolid.com>
2014-07-29 17:10:16 -0700

    Change to use the new config type.

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ console.log('starting');
 var express = require('express'),
     app = express(),
     api = require('grasshopper-api'),
+    config = require('./ghapi.json'),
     PORT = process.env.PORT || 3000;
 
-app.use(api('use proxy').ghApi);
+app.use(api(config).router);
 app.use(express.static(__dirname + '/public'));
 console.log('litening on: ' + PORT);
 app.listen(PORT);
