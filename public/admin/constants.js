4cd3b06809224104a4db907891f78f80b7ede861
Valentine Nesterov <nesterov.valentine@gmail.com>
2014-08-01 14:08:36 -0700

    Build: 24 Branch: kirby  0.27.9 SHA: 13eaf65

---
+++ public/admin/constants.js
@@ -0,0 +1,121 @@
+/*global define*/
+define([], function() {
+    'use strict';
+
+    var apiEndpoint = '',
+        version = '0.27.8',
+        libraryVersions = '[{"name":"ScrollToFixed","version":"1.0.6"},{"name":"SparkMD5","version":"0.0.5"},{"name":"ace","version":"1.1.3"},{"name":"ckeditor","version":"4.3.5"},{"name":"datetimepicker","version":"2.3.2"},{"name":"font-awesome","version":"4.1.0"},{"name":"foundation","version":"5.0.2"},{"name":"jquery.ui","version":"1.10.4"},{"name":"js-base64","version":"2.1.2"},{"name":"masseuse","version":"2.3.0"},{"name":"moment","version":"2.5.1"},{"name":"requirejs-text","version":"2.0.10"},{"name":"select2","version":"3.5.1"}]';
+
+    return {
+        version : version,
+        libraryVersions: libraryVersions,
+        api : {
+            base : {
+                url : apiEndpoint
+            },
+            login : {
+                url : apiEndpoint + '/token'
+            },
+            logout : {
+                url : apiEndpoint + '/token/logout'
+            },
+            user : {
+                url : apiEndpoint + '/user'
+            },
+            users : {
+                url : apiEndpoint + '/users'
+            },
+            newUser : {
+                url : apiEndpoint + '/users'
+            },
+            contentTypes : {
+                url : apiEndpoint + '/contenttypes'
+            },
+            node : {
+                url : apiEndpoint + '/node'
+            },
+            nodesChildren : {
+                url : apiEndpoint + '/node/:id/children'
+            },
+            nodesContent : {
+                url : apiEndpoint + '/node/:id/content'
+            },
+            assets : {
+                url : apiEndpoint + '/node/:id/assets'
+            },
+            nodesContentTypes : {
+                url : apiEndpoint + '/node/:id/contenttype'
+            },
+            content : {
+                url : apiEndpoint + '/content'
+            },
+            contentQuery : {
+                url : apiEndpoint + '/content/query'
+            },
+            version : {
+                url : apiEndpoint + '/system'
+            },
+            google : {
+                url : apiEndpoint + '/googleurl'
+            }
+        },
+        internalRoutes : {
+            user : '#user',
+            users : '#users',
+            addUser : '#addUser',
+            sysInfo: '#sysinfo',
+            contentTypes : '#contentTypes',
+            newContentType : '#contentTypes/new',
+            contentTypeDetail : '#contentTypes/:id',
+            content : '#items',
+            contentDetail : '#item/:id',
+            nodeDetail : '#items/nodeid/:id',
+            login : '#login',
+            logout : '#logout',
+            about : '#about',
+            createFolder : '#items/nodeid/:id/createFolder',
+            addContent : '#items/nodeid/:id/createContent',
+            createAssets : '#items/nodeid/:id/createAssets',
+            forbidden : '#forbidden'
+        },
+        timeouts: {
+            showSpinnerLoadingTimeout: 2000
+        },
+        fileExtensionsMap : {
+            pdf : 'fa-file-pdf-o',
+            doc : 'fa-file-word-o',
+            docx : 'fa-file-word-o',
+            xsl : 'fa-file-excel-o',
+            xslx : 'fa-file-excel-o',
+            txt : 'fa-file-text',
+            zip : 'fa-file-archive-o',
+            psd : 'fa-file-image-o',
+            ai : 'fa-file-image-o',
+            swf : 'fa-file-video-o'
+        },
+        imageExtensions : ['jpg', 'jpeg', 'png', 'bmp', 'webp', 'svg', 'gif'],
+        contentSearchThrottle: 1000,
+        pagination : {
+            defaultLimit : 25,
+            defaultSkip : 1,
+            defaultPagesLimit : 7,
+            defaultPageSize : [25, 50, 100, 'all'],
+            defaultAllLimit : 100000
+        },
+        controlKeyCodeMap : {
+            13 : 'enter',
+            16 : 'shift',
+            17 : 'cntrl',
+            18 : 'alt',
+            20 : 'capsLock',
+            27 : 'esc',
+            37 : 'lArr',
+            38 : 'tArr',
+            39 : 'rArr',
+            40 : 'bArr',
+            91 : 'leftCMD',
+            93 : 'rightCMD'
+        }
+    };
+
+});
