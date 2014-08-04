74cd75856dd85428eefd1dd9476f3c9bc5d3a054
Andrew Gurinovich <altmind@gmail.com>
2014-08-04 12:45:55 -0700

    Fixed images references for local build

--- grunt/config/processTemplateResourceRefs.js
+++ grunt/config/processTemplateResourceRefs.js
@@ -9,8 +9,8 @@ module.exports = function (grunt) {
             development: {
                 // Files with RESOURCEREF should be specified there
                 files: [
-                    {expand: true, src:grunt.config('buildDirectory') + '/**.html'},
-                    {expand: true, src:grunt.config('buildDirectory') + '/**.js'}/*,
+                    {expand: true, src:grunt.config('buildDirectory') + '/**/*.html'},
+                    {expand: true, src:grunt.config('buildDirectory') + '/**/*.js'}/*,
                     {expand: true, src:grunt.config('buildDirectory') + '*//**.css'}*/
                 ]
                 // save some time by specifying files to process explicitly
