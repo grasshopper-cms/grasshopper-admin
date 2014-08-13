4fdffb646dc63d62e24fe62016883ee5e687d2e9
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-13 11:29:11 -0700

    Formatting Fix.

--- app/plugins/author/view.js
+++ app/plugins/author/view.js
@@ -11,27 +11,35 @@ define(['grasshopperBaseView', 'underscore', 'jquery', 'require', 'resources'],
 
         function beforeRender($deferred) {
             var self = this;
-            self.model.get('users').fetch().fail(function (jqXHR, status, error) {
-                self.model.get('users').reset([ self.app.user ]);
-                self.model.set('no_permissions', true);
-                $deferred.resolve();
-            }).done(function () {
-                var filteredUsers = self.model.get('users').filter(function (item) {
-                    return _.contains(['admin', 'editor', 'author'], item.get('role'));
+
+            self.model.get('users').fetch()
+                .fail(function (jqXHR, status, error) {
+                    self.model.get('users').reset([ self.app.user ]);
+                    self.model.set('no_permissions', true);
+                    $deferred.resolve();
+                })
+                .done(function () {
+                    var filteredUsers = self.model.get('users').filter(function (item) {
+                        return _.contains(['admin', 'editor', 'author'], item.get('role'));
+                    });
+                    self.model.get('users').reset(filteredUsers);
+                    $deferred.resolve();
                 });
-                self.model.get('users').reset(filteredUsers);
-                $deferred.resolve();
-            });
         }
 
         function afterRender() {
-            var value = this.model.get('value'), selectEl = this.$('select');
-            selectEl.select2({
-                containerCssClass: 'authorDropdownSelectContainer',
-                dropdownCssClass: 'authorDropdownSelectDrop',
-                placeholder: resources.plugins.author.selectUser,
-                allowClear: true
-            }).on('change', _changeValue.bind(this));
+            var value = this.model.get('value'),
+                selectEl = this.$('select');
+
+            selectEl.select2(
+                {
+                    containerCssClass : 'authorDropdownSelectContainer',
+                    dropdownCssClass : 'authorDropdownSelectDrop',
+                    placeholder : resources.plugins.author.selectUser,
+                    allowClear : true
+                })
+                .on('change', _changeValue.bind(this));
+
             if (value && typeof(value) === 'object') {
                 selectEl.select2('val', value.id);
             } else if (value && typeof(value) === 'string') {
@@ -40,9 +48,13 @@ define(['grasshopperBaseView', 'underscore', 'jquery', 'require', 'resources'],
         }
 
         function _getValueStruct(id) {
-            var foundUser=this.model.get('users').findWhere({_id: id});
+            var foundUser = this.model.get('users').findWhere({ _id : id });
+
             if (foundUser){
-                return {id: id, displayName: foundUser.get('displayName')};
+                return {
+                    id : id,
+                    displayName : foundUser.get('displayName')
+                };
             }
             // if no user found, return undefined
         }
@@ -50,6 +62,5 @@ define(['grasshopperBaseView', 'underscore', 'jquery', 'require', 'resources'],
         function _changeValue(e) {
             this.model.set('value', _getValueStruct.call(this, e.val));
         }
-
 
     });
