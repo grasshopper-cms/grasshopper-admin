0941e63aa5295821de4d6c0a0b32ae2414d91438
Andrew Gurinovich <altmind@gmail.com>
2014-08-18 13:50:13 -0700

    GRAS-1458 The user's displayname should be added to the user list view

--- app/resources.js
+++ app/resources.js
@@ -317,7 +317,7 @@ define({
     key: 'Key',
     label : 'Label',
     login : 'Login',
-    displayname : 'Display Name',
+    displayname : 'Display name',
     log_in : 'Log In',
     logInWithGoogle : 'Login with Google',
     log_out : 'Log Out',
