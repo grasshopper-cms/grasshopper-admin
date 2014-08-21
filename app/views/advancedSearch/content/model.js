43c5f65d172baaa4ca1c9f054e26308b702b5f31
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-20 21:13:03 -0700

    Removing unused method.

--- app/views/advancedSearch/content/model.js
+++ app/views/advancedSearch/content/model.js
@@ -2,15 +2,8 @@ define(['grasshopperModel', 'resources'], function (Model, resources) {
     'use strict';
 
     return Model.extend({
-        initialize : initialize,
         defaults : {
             resources : resources
         }
     });
-
-    function initialize() {
-        Model.prototype.initialize.apply(this, arguments);
-
-        console.log('YEAH YEAH YEAH YEAH YEAH YEAH EYAH');
-    }
 });
