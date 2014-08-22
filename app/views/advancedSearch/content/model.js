f3f603899416a82b870ceff33c33328afcf2147a
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-21 18:41:52 -0700

    Prepare possible fields to search with, and add comparators.

--- app/views/advancedSearch/content/model.js
+++ app/views/advancedSearch/content/model.js
@@ -1,9 +1,11 @@
-define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants'],
-    function (Model, resources, GrasshopperCollection, constants) {
+define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants', 'underscore'],
+    function (Model, resources, GrasshopperCollection, constants, _) {
         'use strict';
 
         return Model.extend({
             initialize : initialize,
+            preparePossibleContentFields : preparePossibleContentFields,
+            preparePossibleFieldComparators : preparePossibleFieldComparators,
             defaults : {
                 resources : resources,
                 inTypes : []
@@ -20,5 +22,24 @@ define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants'],
             this.set('nodesCollection', new (GrasshopperCollection.extend({
                 url : constants.api.nodesChildrenDeep.url.replace(':id', 0)
             }))());
+        }
+
+        function preparePossibleContentFields() {
+            var possibleFields = [];
+
+            this.get('contentTypeCollection').each(function(contentType){
+                _.each(contentType.get('fields'), function(field) {
+                    possibleFields.push({
+                        label : field.label,
+                        _id : field._id
+                    });
+                });
+            });
+
+            this.set('possibleFields', possibleFields);
+        }
+
+        function preparePossibleFieldComparators() {
+            this.set('possibleQueryComparators', constants.possibleQueryComparators);
         }
     });
