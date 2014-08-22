a4645cdd096871dfb0896fefaecdc6c8d601c849
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-22 15:32:00 -0700

    you can now add and remove filter options.

--- app/views/advancedSearch/content/view.js
+++ app/views/advancedSearch/content/view.js
@@ -5,7 +5,13 @@ define(['grasshopperBaseView', 'advancedSearch/content/config', 'jquery'],
 
         return GrasshopperBaseView.extend({
             defaultOptions : config,
-            beforeRender : beforeRender
+            beforeRender : beforeRender,
+            addNewFilterToFiltersCollection : addNewFilterToFiltersCollection,
+            addContentTypeToInTypesCollection : addContentTypeToInTypesCollection,
+            removeContentTypeFromInTypesCollection : removeContentTypeFromInTypesCollection,
+            addNodeToInNodesCollection : addNodeToInNodesCollection,
+            removeNodeFromInNodesCollection : removeNodeFromInNodesCollection,
+            resetNewFilter : resetNewFilter
         });
 
         function beforeRender($deferred) {
@@ -13,8 +19,41 @@ define(['grasshopperBaseView', 'advancedSearch/content/config', 'jquery'],
                 this.model.get('contentTypeCollection').fetch(),
                 this.model.get('nodesCollection').fetch()
             )
-                .then(this.model.preparePossibleFieldComparators.bind(this.model))
                 .then($deferred.resolve);
+        }
+
+        function addNewFilterToFiltersCollection() {
+            this.model.get('filtersCollection').add(this.model.get('newFilterModel').toJSON());
+            this.resetNewFilter();
+        }
+
+        function resetNewFilter() {
+            this.model.get('newFilterModel').clear();
+            this.$('#newFilterKeys').add('#newFilterComparator').select2('val', '');
+        }
+
+        function addContentTypeToInTypesCollection(select2Event) {
+            this.$('#selectContentTypes').select2('val', '');
+            this.model.get('inTypesCollection').add({
+                label : select2Event.added.text,
+                _id : select2Event.added.id
+            });
+        }
+
+        function removeContentTypeFromInTypesCollection(event, context) {
+            this.model.get('inTypesCollection').remove(context.contenttype);
+        }
+
+        function addNodeToInNodesCollection(select2Event) {
+            this.$('#selectNodes').select2('val', '');
+            this.model.get('inNodesCollection').add({
+                label : select2Event.added.text,
+                _id : select2Event.added.id
+            });
+        }
+
+        function removeNodeFromInNodesCollection(event, context) {
+            this.model.get('inNodesCollection').remove(context.node);
         }
 
     });
