b151fec286ed0026b6a218e35a98b9b05bd50249
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-21 17:53:24 -0700

    Advanced Search Model now has content types and nodes.

--- app/views/advancedSearch/content/model.js
+++ app/views/advancedSearch/content/model.js
@@ -1,9 +1,24 @@
-define(['grasshopperModel', 'resources'], function (Model, resources) {
-    'use strict';
+define(['grasshopperModel', 'resources', 'grasshopperCollection', 'constants'],
+    function (Model, resources, GrasshopperCollection, constants) {
+        'use strict';
 
-    return Model.extend({
-        defaults : {
-            resources : resources
+        return Model.extend({
+            initialize : initialize,
+            defaults : {
+                resources : resources,
+                inTypes : []
+            }
+        });
+
+        function initialize() {
+            Model.prototype.initialize.apply(this, arguments);
+
+            this.set('contentTypeCollection', new (GrasshopperCollection.extend({
+                url : constants.api.contentTypes.url
+            }))());
+
+            this.set('nodesCollection', new (GrasshopperCollection.extend({
+                url : constants.api.nodesChildrenDeep.url.replace(':id', 0)
+            }))());
         }
     });
-});
