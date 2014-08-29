6bcae0e982a9c8d8bcbed302966c6bf6e2d4ad3e
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-08-28 19:05:59 -0700

    Advanced Content Search now has access to the query string.

--- app/views/advancedSearch/model.js
+++ app/views/advancedSearch/model.js
@@ -1,10 +1,29 @@
-define(['grasshopperModel', 'resources'],
-    function(Model, resources) {
-        'use strict';
+define(['grasshopperModel', 'resources', 'advancedSearch/content/model'], function(Model, resources, AdvancedSearchContentModel) {
+    'use strict';
 
-        return Model.extend({
-            defaults : {
-                resources : resources
-            }
-        });
+    return Model.extend({
+        initialize : initialize,
+        defaults : {
+            resources : resources,
+            searchType : null,
+            queryOptions : {
+                filters : [],
+                nodes : [],
+                types : [],
+                options : {
+                    limit : 1000,
+                    skip : 0
+                }
+            },
+            contentSearchModel : null
+        }
     });
+
+    function initialize() {
+        Model.prototype.initialize.apply(this, arguments);
+
+        this.set('contentSearchModel', new AdvancedSearchContentModel());
+
+        this.get('contentSearchModel').set('queryOptions', this.get('queryOptions'));
+    }
+});
