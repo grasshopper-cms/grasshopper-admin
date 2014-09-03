1b5215b542d796e25cbfc7b6682eae1d57c70e24
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-09-03 10:53:22 -0700

    Removing non-used binder.

--- app/appBinders.js
+++ app/appBinders.js
@@ -29,24 +29,6 @@ define(['jquery', 'underscore', 'masseuse',
                 },
                 publish : true
             },
-            editable : {
-                routine: function(el, model) {
-                    this.view.binders.text.call(this, el, model);
-                },
-                bind : function(el) {
-                    var $el = $(el);
-                    if(!$el.attr('contenteditable')) {
-                        $el.attr('contenteditable', true);
-                    }
-
-                    el.addEventListener('keypress', _callback.bind(this, el), false);
-                    el.addEventListener('blur', _callback.bind(this, el), false);
-                },
-                unbind: function(el) {
-                    el.removeEventListener('keypress', _callback.bind(this, el), false);
-                    el.removeEventListener('blur', _callback.bind(this, el), false);
-                }
-            },
             'move-to' : function(el, selector) {
                 var $selector = $(selector);
                 $selector.append($(el).contents());
@@ -114,14 +96,6 @@ define(['jquery', 'underscore', 'masseuse',
                     });
             } else {
                 $el.hide();
-            }
-        }
-
-        function _callback(el, evt) {
-            // listen for the enter key or Blur to save to the model.
-            if(evt.keyCode === 13 || evt.type === 'blur') {
-                this.view.adapters[':'].publish(
-                    this.model, this.keypath.substring(this.keypath.indexOf(':')+1), el.textContent);
             }
         }
     });
