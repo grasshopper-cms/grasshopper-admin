4797897578ac4feb99989eccf965ad442f16eef3
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2015-09-04 16:02:26 -0700

    Fixing up the index js a little bit. A little more clean, a little less tricky.

--- app/server/index.js
+++ app/server/index.js
@@ -11,8 +11,12 @@ api = api();
 app.use(api.ghApi || api.router);
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/admin*?', function(request, response) {
-    response.sendfile(path.join(__dirname, '..', 'client', 'index.html'));
+app.get('/admin', function(request, response) {
+    response.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.get('*', function(request, response) {
+    response.redirect('/admin');
 });
 
 app.listen(port, function() {
